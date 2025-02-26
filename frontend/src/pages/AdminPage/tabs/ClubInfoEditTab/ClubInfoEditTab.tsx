fbac8cb4de742b919444ebdd971c1657618d331c
Junseo Kim <semiwest2@gmail.com>
2025-02-26 19:02:22 +0900

    refactor: AccountEditTab 및 ClubInfoEditTab에서 상태 변수 이름 수정 및 불필요한 주석 제거

--- frontend/src/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab.tsx
+++ frontend/src/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab.tsx
@@ -2,24 +2,22 @@ import React, { useState } from "react";
 import InputField from "@/components/InputField/InputField";
 
 const ClubInfoEditTab = () => {
-    const [name, setName] = useState(""); // 동아리 명 (clubName -> name)
-    const [clubPresidentName, setClubPresidentName] = useState(""); // 회장 이름 (nickname -> clubPresidentName)
-    const [introduction, setIntroduction] = useState(""); // 동아리 설명 (clubDescription -> description)
-    const [telephoneNumber, setTelephoneNumber] = useState(""); // 전화번호 (phoneNumber -> telephoneNumber)
+    const [clubName, setClubName] = useState("");
+    const [clubPresidentName, setClubPresidentName] = useState("");
+    const [introduction, setIntroduction] = useState("");
+    const [telephoneNumber, setTelephoneNumber] = useState("");
 
     return (
         <div>
-            {/* 동아리 명 */}
             <InputField
                 label="동아리 명"
                 placeholder="동아리 명을 입력해주세요"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={clubName}
+                onChange={(e) => setClubName(e.target.value)}
                 maxLength={80}
                 showMaxChar={true}
             />
 
-            {/* 회장 이름 */}
             <InputField
                 label="회장 정보"
                 placeholder="동아리 대표의 이름을 입력해주세요"
@@ -29,7 +27,6 @@ const ClubInfoEditTab = () => {
                 onChange={(e) => setClubPresidentName(e.target.value)}
             />
 
-            {/* 전화번호 */}
             <InputField
                 placeholder="전화번호를 입력해주세요"
                 type="text"
@@ -39,8 +36,6 @@ const ClubInfoEditTab = () => {
             />
 
 
-
-            {/* 한줄소개 */}
             <InputField
                 label="한줄소개"
                 placeholder="한줄소개를 입력해주세요"
