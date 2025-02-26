6c9d37b81749db15c9d29b514df54db80841e898
Junseo Kim <semiwest2@gmail.com>
2025-02-26 18:56:30 +0900

    feat: 동아리 정보 편집 탭에 입력 필드 예시 추가 및 상태 관리 구현

--- frontend/src/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab.tsx
+++ frontend/src/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
+import InputField from "@/components/InputField/InputField";
 
 const ClubInfoEditTab = () => {
+    const [name, setName] = useState(""); // 동아리 명 (clubName -> name)
+    const [clubPresidentName, setClubPresidentName] = useState(""); // 회장 이름 (nickname -> clubPresidentName)
+    const [introduction, setIntroduction] = useState(""); // 동아리 설명 (clubDescription -> description)
+    const [telephoneNumber, setTelephoneNumber] = useState(""); // 전화번호 (phoneNumber -> telephoneNumber)
+
     return (
         <div>
-            <h1>Club Info Edit Tab</h1>
+            {/* 동아리 명 */}
+            <InputField
+                label="동아리 명"
+                placeholder="동아리 명을 입력해주세요"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                maxLength={80}
+                showMaxChar={true}
+            />
+
+            {/* 회장 이름 */}
+            <InputField
+                label="회장 정보"
+                placeholder="동아리 대표의 이름을 입력해주세요"
+                type="text"
+                value={clubPresidentName}
+                maxLength={5}
+                onChange={(e) => setClubPresidentName(e.target.value)}
+            />
+
+            {/* 전화번호 */}
+            <InputField
+                placeholder="전화번호를 입력해주세요"
+                type="text"
+                maxLength={13}
+                value={telephoneNumber}
+                onChange={(e) => setTelephoneNumber(e.target.value)}
+            />
+
+
+
+            {/* 한줄소개 */}
+            <InputField
+                label="한줄소개"
+                placeholder="한줄소개를 입력해주세요"
+                type="text"
+                maxLength={40}
+                showMaxChar={true}
+                value={introduction}
+                onChange={(e) => setIntroduction(e.target.value)}
+            />
         </div>
     );
 };
