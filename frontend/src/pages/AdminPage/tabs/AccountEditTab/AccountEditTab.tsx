98918ac8f2c2955e0c649f3834f17374c363656e
Junseo Kim <semiwest2@gmail.com>
2025-02-26 18:56:56 +0900

    feat: 계정 정보 수정 탭에 아이디 및 비밀번호 입력 필드 추가 (비활성화 상태)

--- frontend/src/pages/AdminPage/tabs/AccountEditTab/AccountEditTab.tsx
+++ frontend/src/pages/AdminPage/tabs/AccountEditTab/AccountEditTab.tsx
@@ -9,6 +9,24 @@ const AccountEditTab = () => {
         <div>
             <h1>계정 정보 수정</h1>
 
+            <InputField
+                label="아이디"
+                placeholder="아이디를 입력해주세요"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                maxLength={20}
+                disabled={true}
+            />
+
+            <InputField
+                label="비밀번호"
+                placeholder="비밀번호를 입력해주세요"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                maxLength={20}
+                disabled={true}
+            />
 
 
             <InputField
