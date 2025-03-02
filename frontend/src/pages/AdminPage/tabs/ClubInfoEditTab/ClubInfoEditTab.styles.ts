b39d14cf999e664a5ebfe90168fb33fa34c2ada7
Junseo Kim <semiwest2@gmail.com>
2025-03-02 14:28:26 +0900

    feat: 동아리 정보 및 태그 수정 탭 - 스타일 추가

---
+++ frontend/src/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab.styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InfoTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  letter-spacing: 0;
+  margin-bottom: 46px;
+`;
+
+export const InfoGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px; 
+  margin-bottom: 140px;
+`;
+
+export const PresidentContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: flex-end;
+`;
+
+export const TagEditGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px; 
+`;
