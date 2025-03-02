8ee431b39cc6a7e36b1637394fb74e1e596dd77d
Junseo Kim <semiwest2@gmail.com>
2025-03-02 14:22:40 +0900

    feat: 버튼 컴포넌트 제작

---
+++ frontend/src/components/common/Button/Button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ButtonProps {
+    width?: string;
+    children: React.ReactNode;
+    onClick: () => void;
+}
+
+const StyledButton = styled.button<ButtonProps>`
+  background-color: #3A3A3A;
+  color: #ffffff;
+  height: 45px;
+  border-radius: 10px;
+  border: none;
+  font-weight: 600;
+  font-size: 1.25rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  width: ${({ width }) => width || "auto"};
+
+  &:hover {
+    background-color: #333333;
+  }
+`;
+
+function Button({ width, children, onClick }: ButtonProps) {
+    return (
+        <StyledButton width={width} onClick={onClick}>
+            {children}
+        </StyledButton>
+    );
+}
+
+export default Button;
