ce1c0eae1eb416a863be5192f4cd21a44cd06bcb
Junseo Kim <semiwest2@gmail.com>
2025-03-06 04:38:23 +0900

    feat: RecruitEditTab 컴포넌트에 모집 기간 설정 기능 추가

--- frontend/src/pages/AdminPage/tabs/RecruitEditTab/RecruitEditTab.tsx
+++ frontend/src/pages/AdminPage/tabs/RecruitEditTab/RecruitEditTab.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from "@/pages/AdminPage/components/Calendar/Calendar";
+import { parseRecruitmentPeriod } from "@/utils/stringToDate";
+
 
 const RecruitEditTab = () => {
-  return (
-    <div>
-      <h1>Recruit Edit Tab</h1>
-      {/* Add your content here */}
-    </div>
-  );
+    // API 예상 데이터
+    const recruitmentPeriod = "2025.01.04 00:00 ~ 2025.06.04 00:00";
+
+    const { recruitmentStart: initialStart, recruitmentEnd: initialEnd } = parseRecruitmentPeriod(recruitmentPeriod);
+    const [recruitmentStart, setRecruitmentStart] = useState<Date | null>(initialStart);
+    const [recruitmentEnd, setRecruitmentEnd] = useState<Date | null>(initialEnd);
+    
+
+    return (
+        <div>
+            <h2>모집 기간 설정</h2>
+            <Calendar
+                recruitmentStart={recruitmentStart}
+                recruitmentEnd={recruitmentEnd}
+                onChangeStart={setRecruitmentStart}
+                onChangeEnd={setRecruitmentEnd}
+            />
+        </div>
+    );
 };
 
 export default RecruitEditTab;
