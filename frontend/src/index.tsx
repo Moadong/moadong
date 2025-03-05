ab5e229f3999fc5aa734300a27c2dd1bab2921c1
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-06 02:42:52 +0900

    feat: index.tsx에 mixpanel적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN || '', {
+  ip: false,
+  debug: true,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
