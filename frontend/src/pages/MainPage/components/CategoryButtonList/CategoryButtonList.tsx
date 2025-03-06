c1dfe0ff96dc20c149a1c3e863604761df1ff358
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-06 10:09:52 +0900

    feat: 카테고리 버튼에 track추가
    - 각 버튼에 다른 이벤트로 tracking

--- frontend/src/pages/MainPage/components/CategoryButtonList/CategoryButtonList.tsx
+++ frontend/src/pages/MainPage/components/CategoryButtonList/CategoryButtonList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import mixpanel from 'mixpanel-browser';
 import * as Styled from './CategoryButtonList.styles';
 import iconAll from '@/assets/images/icon_all.png';
 import iconVolunteer from '@/assets/images/icon_volunteer.png';
@@ -12,6 +13,7 @@ interface Category {
   id: string;
   name: string;
   icon: string;
+  eventName: string;
 }
 
 interface CategoryButtonListProps {
@@ -19,22 +21,63 @@ interface CategoryButtonListProps {
 }
 
 const clubCategories: Category[] = [
-  { id: 'all', name: '전체', icon: iconAll },
-  { id: 'volunteering', name: '봉사', icon: iconVolunteer },
-  { id: 'religion', name: '종교', icon: iconReligion },
-  { id: 'hobby', name: '취미교양', icon: iconHobby },
-  { id: 'study', name: '학술', icon: iconAcademic },
-  { id: 'sport', name: '운동', icon: iconSport },
-  { id: 'performance', name: '공연', icon: iconPerformance },
+  { id: 'all', name: '전체', icon: iconAll, eventName: 'Category_All_Clicked' },
+  {
+    id: 'volunteering',
+    name: '봉사',
+    icon: iconVolunteer,
+    eventName: 'Category_Volunteering_Clicked',
+  },
+  {
+    id: 'religion',
+    name: '종교',
+    icon: iconReligion,
+    eventName: 'Category_Religion_Clicked',
+  },
+  {
+    id: 'hobby',
+    name: '취미교양',
+    icon: iconHobby,
+    eventName: 'Category_Hobby_Clicked',
+  },
+  {
+    id: 'study',
+    name: '학술',
+    icon: iconAcademic,
+    eventName: 'Category_Study_Clicked',
+  },
+  {
+    id: 'sport',
+    name: '운동',
+    icon: iconSport,
+    eventName: 'Category_Sport_Clicked',
+  },
+  {
+    id: 'performance',
+    name: '공연',
+    icon: iconPerformance,
+    eventName: 'Category_Performance_Clicked',
+  },
 ];
 
 const CategoryButtonList = ({ onCategorySelect }: CategoryButtonListProps) => {
+  const handleCategoryClick = (category: Category) => {
+    mixpanel.track(category.eventName, {
+      category_id: category.id,
+      category_name: category.name,
+      timestamp: Date.now(),
+      url: window.location.href,
+    });
+
+    onCategorySelect(category.id);
+  };
+
   return (
     <Styled.CategoryButtonContainer>
       {clubCategories.map((category) => (
         <Styled.CategoryButton
           key={category.id}
-          onClick={() => onCategorySelect(category.id)}>
+          onClick={() => handleCategoryClick(category)}>
           <img src={category.icon} alt={category.name} />
           <span>{category.name}</span>
         </Styled.CategoryButton>
