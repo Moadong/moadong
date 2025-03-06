8c99877282bd17c7963a5f11247747ed98657716
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-06 22:09:05 +0900

    fix: track_pageview 삭제

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,6 @@ import mixpanel from 'mixpanel-browser';
 mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN || '', {
   ip: false,
   debug: true,
-  track_pageview: true,
 });
 
 const root = ReactDOM.createRoot(
