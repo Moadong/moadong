273942cae13acd6be8c8a51dd525e82ed33f28d4
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-06 10:07:51 +0900

    feat: 검색아이콘 클릭에 track 추가

--- frontend/src/components/common/SearchBox/SearchBox.tsx
+++ frontend/src/components/common/SearchBox/SearchBox.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useMixpanelTrack from '@/hooks/useMixpanelTrack';
 import * as Styled from './SearchBox.styles';
 import SearchIcon from '@/assets/images/searchIcon.png';
 
 const SearchBox = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const trackEvent = useMixpanelTrack();
+
+  const handleSearchClick = () => {
+    if (!searchKeyword.trim()) {
+      console.log('검색어가 비어 있어 트래킹하지 않습니다.');
+      return;
+    }
+
+    trackEvent('Search Button Clicked');
+
+    console.log(`검색 실행: ${searchKeyword}`);
+  };
+
   return (
     <Styled.SearchBoxStyles>
-      <Styled.SearchInputStyles placeholder='어떤 동아리를 찾으세요?' />
-      <Styled.SearchButton>
-        <img src={SearchIcon} />
+      <Styled.SearchInputStyles
+        placeholder='어떤 동아리를 찾으세요?'
+        onChange={(e) => setSearchKeyword(e.target.value)}
+      />
+      <Styled.SearchButton onClick={handleSearchClick}>
+        <img src={SearchIcon} alt='Search Button' />
       </Styled.SearchButton>
     </Styled.SearchBoxStyles>
   );
