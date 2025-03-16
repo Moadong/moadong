bf0874caf61805a474c12df2a08b6825b456e286
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-17 01:27:56 +0900

    refactor: clubDetail 필드명 변경
    - presidentName -> clubPresidentName
    - presidentPhoneNumber -> telephoneNumber

--- frontend/src/types/club.ts
+++ frontend/src/types/club.ts
@@ -13,8 +13,8 @@ export interface ClubDetail extends Club {
   state: string;
   feeds: string[];
   description: string;
-  presidentName: string;
-  presidentPhoneNumber: string;
+  clubPresidentName: string;
+  telephoneNumber: string;
   recruitmentPeriod: string;
   recruitmentTarget: string;
 }
