89a2ba81006146964288b0a508178058e6adfe41
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-22 00:17:18 +0900

    fix: 데드라인 마감으로 임시 수정

--- frontend/src/pages/ClubDetailPage/components/DeadlineBadge/DeadlineBadge.tsx
+++ frontend/src/pages/ClubDetailPage/components/DeadlineBadge/DeadlineBadge.tsx
@@ -3,8 +3,9 @@ import * as Styled from './DeadlineBadge.styles';
 
 const DeadlineBadge = () => {
   return (
+    // []FIXME: 모집마감 일자 들고와야 함
     <Styled.DeadlineBadgeWrapper>
-      <Styled.DeadlineBadgeText>D-10</Styled.DeadlineBadgeText>
+      <Styled.DeadlineBadgeText>마감</Styled.DeadlineBadgeText>
     </Styled.DeadlineBadgeWrapper>
   );
 };
