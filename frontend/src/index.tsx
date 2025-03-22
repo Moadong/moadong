598a3527bd70538f9e95e8f3d1abb28ff1bd08ab
Junseo Kim <semiwest2@gmail.com>
2025-03-22 17:27:13 +0900

    refactor: mixpanel init 디버깅 코드 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import mixpanel from 'mixpanel-browser';
 
-const mixpanelToken = process.env.REACT_APP_MIXPANEL_TOKEN;
-
-if (mixpanelToken) {
-  try {
-    mixpanel.init(mixpanelToken, {
-      ip: false,
-      debug: true,
-    });
-
-    console.log('✅ Mixpanel initialized successfully!');
-    mixpanel.track('App Initialized');
-  } catch (error) {
-    console.error('❌ Mixpanel initialization failed:', error);
-  }
-} else {
-  console.warn('⚠️ Mixpanel token is missing.');
+if (process.env.REACT_APP_MIXPANEL_TOKEN) {
+  mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN, {
+    ip: false,
+    debug: true,
+  });
 }
 
 const root = ReactDOM.createRoot(
