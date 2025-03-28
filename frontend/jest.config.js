2a1f49c429dfb8b582951a63ab61862c985c06dd
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-03-28 13:24:39 +0900

    fix: jest 테스트 환경 node에서 Jsdom으로 변경

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\.tsx?$': ['ts-jest', {}],
   },
