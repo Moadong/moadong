f4cba2b3fda49befdfa7a863d68392c1632b3b11
FhRh <yiyop@naver.com>
2025-04-03 22:26:37 +0900

    feat : 동아리를 조회순으로 조회한다

--- backend/src/main/java/moadong/club/repository/ClubRepository.java
+++ backend/src/main/java/moadong/club/repository/ClubRepository.java
@@ -2,7 +2,6 @@ package moadong.club.repository;
 
 import java.util.List;
 import java.util.Optional;
-
 import moadong.club.entity.Club;
 import moadong.club.enums.ClubState;
 import org.bson.types.ObjectId;
@@ -12,10 +11,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClubRepository extends MongoRepository<Club, String> {
+
     Optional<Club> findClubById(ObjectId id);
+
     Optional<List<Club>> findClubByState(ClubState clubState);
 
     Optional<Club> findClubByUserId(String userId);
+
     @Query("{'division': {$regex: '^?0$', $options: 'i'}}")
     Optional<List<Club>> findClubByDivisionIgnoreCaseExact(String division);
 
@@ -23,14 +25,20 @@ public interface ClubRepository extends MongoRepository<Club, String> {
     Optional<List<Club>> findClubByCategoryIgnoreCaseExact(String category);
 
     @Query("{'state': ?0, 'category': {$regex: '^?1$', $options: 'i'}}")
-    Optional<List<Club>> findClubByStateAndCategoryIgnoreCaseExact(ClubState clubState, String category);
+    Optional<List<Club>> findClubByStateAndCategoryIgnoreCaseExact(ClubState clubState,
+        String category);
 
     @Query("{'state': ?0, 'division': {$regex: '^?1$', $options: 'i'}}")
-    Optional<List<Club>> findClubByStateAndDivisionIgnoreCaseExact(ClubState clubState, String division);
+    Optional<List<Club>> findClubByStateAndDivisionIgnoreCaseExact(ClubState clubState,
+        String division);
 
     @Query("{'category': {$regex: '^?0$', $options: 'i'}, 'division': {$regex: '^?1$', $options: 'i'}}")
-    Optional<List<Club>> findClubByCategoryAndDivisionIgnoreCaseExact(String category, String division);
+    Optional<List<Club>> findClubByCategoryAndDivisionIgnoreCaseExact(String category,
+        String division);
 
     @Query("{'state': ?0, 'category': {$regex: '^?1$', $options: 'i'}, 'division': {$regex: '^?2$', $options: 'i'}}")
-    Optional<List<Club>> findClubByStateAndCategoryAndDivisionIgnoreCaseExact(ClubState clubState, String category, String division);
+    Optional<List<Club>> findClubByStateAndCategoryAndDivisionIgnoreCaseExact(ClubState clubState,
+        String category, String division);
+
+    List<Club> findAllByName(List<String> clubs);
 }
