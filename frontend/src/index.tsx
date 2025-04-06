ac596da628f44a38502d2a6b9acfd5f229ac202f
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-06 21:49:03 +0900

    fix: debug false로 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import mixpanel from 'mixpanel-browser';
 if (process.env.REACT_APP_MIXPANEL_TOKEN) {
   mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN, {
     ip: false,
-    debug: true,
+    debug: false,
   });
 }
 
