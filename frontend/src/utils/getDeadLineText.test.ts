f9224f8b2376c9f5609a8e4436ffdde65ed00c6c
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-09 01:51:54 +0900

    Revert "test: getDeadlineText 함수 테스트에 test.each 활용"
    This reverts commit 4b8476e158cde0e6932e2a73686e7d1e2c087659.

--- frontend/src/utils/getDeadLineText.test.ts
+++ frontend/src/utils/getDeadLineText.test.ts
@@ -1,32 +1,34 @@
 import getDeadlineText from './getDeadLineText';
 
 describe('getDeadlineText 함수 테스트', () => {
-  test.each([
-    [
-      new Date('2025-04-01'),
-      new Date('2025-04-10'),
-      new Date('2025-04-10'),
+  it('오늘이 모집 종료일인 경우 D-Day를 반환해야 한다', () => {
+    const recruitmentStart = new Date('2025-04-01');
+    const recruitmentEnd = new Date('2025-04-10');
+    const today = new Date('2025-04-10');
+    expect(getDeadlineText(recruitmentStart, recruitmentEnd, today)).toBe(
       'D-Day',
-    ],
-    [
-      new Date('2025-04-01'),
-      new Date('2025-04-10'),
-      new Date('2025-04-05'),
+    );
+  });
+
+  it('모집 종료일까지 5일 남은 경우 D-5를 반환해야 한다', () => {
+    const recruitmentStart = new Date('2025-04-01');
+    const recruitmentEnd = new Date('2025-04-10');
+    const today = new Date('2025-04-05');
+    expect(getDeadlineText(recruitmentStart, recruitmentEnd, today)).toBe(
       'D-5',
-    ],
-    [
-      new Date('2025-04-01'),
-      new Date('2025-04-10'),
-      new Date('2025-04-11'),
+    );
+  });
+
+  it('오늘이 모집 종료일 이후인 경우 모집 마감을 반환해야 한다', () => {
+    const recruitmentStart = new Date('2025-04-01');
+    const recruitmentEnd = new Date('2025-04-10');
+    const today = new Date('2025-04-11');
+    expect(getDeadlineText(recruitmentStart, recruitmentEnd, today)).toBe(
       '모집 마감',
-    ],
-    [null, null, new Date('2025-04-10'), '모집 마감'],
-  ])(
-    'recruitmentStart: %s, recruitmentEnd: %s, today: %s => %s',
-    (recruitmentStart, recruitmentEnd, today, expectedResult) => {
-      expect(getDeadlineText(recruitmentStart, recruitmentEnd, today)).toBe(
-        expectedResult,
-      );
-    },
-  );
+    );
+  });
+
+  it('모집 기간이 null인 경우 모집 마감을 반환해야 한다', () => {
+    expect(getDeadlineText(null, null)).toBe('모집 마감');
+  });
 });
