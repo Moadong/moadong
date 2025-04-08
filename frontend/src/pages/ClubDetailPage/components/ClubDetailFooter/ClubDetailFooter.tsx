5f3c93cc9418baa88844f26385abdfe313c6d781
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-08 23:43:02 +0900

    feat: 조건부로 ClubApplyButton에 props 전달 로직 추가
    - 모집 마감 상태가 아닌 경우에만 ClubApplyButton에 recruitmentForm과 presidentPhoneNumber 전달
    - deadlineText를 기반으로 조건부 props 처리
    - 코드 가독성과 재사용성을 고려한 로직 개선

--- frontend/src/pages/ClubDetailPage/components/ClubDetailFooter/ClubDetailFooter.tsx
+++ frontend/src/pages/ClubDetailPage/components/ClubDetailFooter/ClubDetailFooter.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import * as Styled from './ClubDetailFooter.styles';
 import DeadlineBadge from '@/pages/ClubDetailPage/components/DeadlineBadge/DeadlineBadge';
 import ClubApplyButton from '@/pages/ClubDetailPage/components/ClubApplyButton/ClubApplyButton';
+import { parseRecruitmentPeriod } from '@/utils/stringToDate';
+import getDeadlineText from '@/utils/getDeadLineText';
 
 interface ClubDetailFooterProps {
   recruitmentPeriod: string;
+  recruitmentForm: string;
+  presidentPhoneNumber: string;
 }
 
-const ClubDetailFooter = ({ recruitmentPeriod }: ClubDetailFooterProps) => {
+const ClubDetailFooter = ({
+  recruitmentPeriod,
+  recruitmentForm,
+  presidentPhoneNumber,
+}: ClubDetailFooterProps) => {
+  const { recruitmentStart, recruitmentEnd } =
+    parseRecruitmentPeriod(recruitmentPeriod);
+
+  const today = useMemo(() => new Date(), []);
+
+  const deadlineText = useMemo(() => {
+    return getDeadlineText(recruitmentStart, recruitmentEnd, today);
+  }, [recruitmentStart, recruitmentEnd, today]);
+
   return (
     <Styled.ClubDetailFooterContainer>
-      <DeadlineBadge recruitmentPeriod={recruitmentPeriod} />
-      <ClubApplyButton />
+      <DeadlineBadge deadlineText={deadlineText} />
+      <ClubApplyButton
+        {...(deadlineText !== '모집 마감' && {
+          recruitmentForm,
+          presidentPhoneNumber,
+        })}
+      />
     </Styled.ClubDetailFooterContainer>
   );
 };
