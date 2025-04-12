ece1dbf2cc970c203bfa1cbbba587b6ea0760546
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-12 20:10:19 +0900

    feat: index.tsx에서 채널톡 실행하기 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import mixpanel from 'mixpanel-browser';
+import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 
 if (process.env.REACT_APP_MIXPANEL_TOKEN) {
   mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN, {
@@ -13,6 +14,11 @@ if (process.env.REACT_APP_MIXPANEL_TOKEN) {
 if (window.location.hostname === 'localhost') {
   mixpanel.disable();
 }
+
+ChannelService.loadScript();
+ChannelService.boot({
+  pluginKey: process.env.REACT_APP_CHANNEL_PLUGIN_KEY!,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
