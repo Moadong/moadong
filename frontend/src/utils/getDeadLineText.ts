7d08ab59bd97b6f3b3341915fe071e425a9f05b2
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-12 15:59:15 +0900

    refactor: date-fns의 내장함수로 변경

--- frontend/src/utils/getDeadLineText.ts
+++ frontend/src/utils/getDeadLineText.ts
@@ -1,19 +1,17 @@
+import { isAfter, isBefore, differenceInCalendarDays } from 'date-fns';
+
 const getDeadlineText = (
   recruitmentStart: Date | null,
   recruitmentEnd: Date | null,
   today: Date = new Date(),
 ): string => {
-  if (
-    recruitmentStart &&
-    recruitmentEnd &&
-    today >= recruitmentStart &&
-    today <= recruitmentEnd
-  ) {
-    const diffTime = recruitmentEnd.getTime() - today.getTime();
-    const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-    return days > 0 ? `D-${days}` : 'D-Day';
-  }
-  return '모집 마감';
+  console.log(recruitmentEnd, recruitmentStart);
+  if (!recruitmentStart || !recruitmentEnd) return '모집 마감';
+  if (isBefore(today, recruitmentStart)) return '모집 전';
+  if (isAfter(today, recruitmentEnd)) return '모집 마감';
+
+  const days = differenceInCalendarDays(recruitmentEnd, today);
+  return days > 0 ? `D-${days}` : 'D-Day';
 };
 
 export default getDeadlineText;
