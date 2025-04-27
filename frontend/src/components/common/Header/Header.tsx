a4e081214118c1a4f0417337e84ca51148d671aa
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-27 20:29:55 +0900

    refactor: 헤더 컴포넌트 통합 및 view 로직으로 분리

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,49 +1,90 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import useMixpanelTrack from '@/hooks/useMixpanelTrack';
+import { useLocation } from 'react-router-dom';
 import * as Styled from './Header.styles';
 import SearchBox from '@/components/common/SearchBox/SearchBox';
-import MainIcon from '@/assets/images/moadong_name_logo.svg';
-import { useSearch } from '@/context/SearchContext';
+import useHeaderService from '@/services/header/useHeaderService';
+import useIsMobile from '@/hooks/useIsMobile';
+import DesktopMainIcon from '@/assets/images/moadong_name_logo.svg';
+import MobileMainIcon from '@/assets/images/logos/moadong_mobile_logo.svg';
+import MenuBar from '@/assets/images/icons/menu_button_icon.svg';
+
+interface MobileHeaderProps {
+  handleHomeClick: (device: 'mobile' | 'desktop') => void;
+  handleMenuClick: () => void;
+}
+
+interface DesktopHeaderProps {
+  isAdminPage: boolean;
+  handleHomeClick: (device: 'mobile' | 'desktop') => void;
+  handleIntroduceClick: () => void;
+}
+
+const MobileHeader = ({
+  handleHomeClick,
+  handleMenuClick,
+}: MobileHeaderProps) => (
+  <Styled.MobileHeaderContainer>
+    <Styled.MobileHeaderWrapper>
+      <Styled.MobileMainIcon>
+        <img
+          src={MobileMainIcon}
+          alt='홈 버튼'
+          onClick={() => handleHomeClick('mobile')}
+        />
+      </Styled.MobileMainIcon>
+      <SearchBox />
+      <Styled.MobileMenu aria-label='메뉴 버튼'>
+        <img src={MenuBar} alt='메뉴 버튼' onClick={handleMenuClick} />
+      </Styled.MobileMenu>
+    </Styled.MobileHeaderWrapper>
+  </Styled.MobileHeaderContainer>
+);
+
+const DesktopHeader = ({
+  isAdminPage,
+  handleHomeClick,
+  handleIntroduceClick,
+}: DesktopHeaderProps) => (
+  <Styled.HeaderStyles>
+    <Styled.HeaderContainer>
+      <Styled.TextCoverStyles>
+        <Styled.LogoButtonStyles>
+          <img
+            src={DesktopMainIcon}
+            alt='홈 버튼'
+            onClick={() => handleHomeClick('desktop')}
+          />
+        </Styled.LogoButtonStyles>
+        {!isAdminPage && (
+          <Styled.IntroduceButtonStyles onClick={handleIntroduceClick}>
+            모아동 소개
+          </Styled.IntroduceButtonStyles>
+        )}
+      </Styled.TextCoverStyles>
+      {!isAdminPage && <SearchBox />}
+    </Styled.HeaderContainer>
+  </Styled.HeaderStyles>
+);
 
 const Header = () => {
-  const { setKeyword, setInputValue } = useSearch();
-  const navigate = useNavigate();
+  const isMobile = useIsMobile();
   const location = useLocation();
-  const trackEvent = useMixpanelTrack();
-
-  const handleHomeClick = () => {
-    navigate('/');
-    setKeyword('');
-    setInputValue('');
-    trackEvent('Home Button Clicked');
-  };
-
-  const handleIntroduceClick = () => {
-    window.location.href =
-      'https://valiant-schooner-12c.notion.site/1a64ac84bab3805287e0cef50b563370';
-    trackEvent('Introduce Button Clicked');
-  };
-
   const isAdminPage = location.pathname.startsWith('/admin');
 
-  return (
-    <Styled.HeaderStyles>
-      <Styled.HeaderContainer>
-        <Styled.TextCoverStyles>
-          <Styled.LogoButtonStyles>
-            <img src={MainIcon} alt='홈 버튼' onClick={handleHomeClick} />
-          </Styled.LogoButtonStyles>
-          {!isAdminPage && (
-            <Styled.IntroduceButtonStyles onClick={handleIntroduceClick}>
-              모아동 소개
-            </Styled.IntroduceButtonStyles>
-          )}
-        </Styled.TextCoverStyles>
+  const { handleHomeClick, handleIntroduceClick, handleMenuClick } =
+    useHeaderService();
 
-        {!isAdminPage && <SearchBox />}
-      </Styled.HeaderContainer>
-    </Styled.HeaderStyles>
+  return isMobile ? (
+    <MobileHeader
+      handleHomeClick={handleHomeClick}
+      handleMenuClick={handleMenuClick}
+    />
+  ) : (
+    <DesktopHeader
+      isAdminPage={isAdminPage}
+      handleHomeClick={handleHomeClick}
+      handleIntroduceClick={handleIntroduceClick}
+    />
   );
 };
 
