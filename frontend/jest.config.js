de0a4b493e3a7a8406a025caa4e30826ad4983c3
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-04-28 02:03:51 +0900

    feat: codecov 설정 추가

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'], // text(콘솔 출력), lcov(Codecov용)
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}', // 소스 폴더
+    '!src/**/*.d.ts', // 타입 선언 파일 제외
+    '!src/**/index.ts', // index 파일 제외
+  ],
 };
