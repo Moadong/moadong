a03eb87c7d6ec85a5040920e5b226a3e3e840235
Zepelown <dbswjdqhd12@gmail.com>
2025-05-03 15:40:20 +0900

    feat: 단위 테스트와 통합 테스트 분리를 위한 어노테이션 추가

---
+++ backend/src/test/java/moadong/util/annotations/IntegrationTest.java
@@ -0,0 +1,20 @@
+package moadong.util.annotations;
+
+import moadong.global.util.TestTypeConstants;
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@Tag(TestTypeConstants.INTEGRATION_TEST)
+public @interface IntegrationTest {
+
+}
---
+++ backend/src/test/java/moadong/util/annotations/UnitTest.java
@@ -0,0 +1,21 @@
+package moadong.util.annotations;
+
+
+
+import moadong.global.util.TestTypeConstants;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag(TestTypeConstants.UNIT_TEST)
+@ExtendWith(MockitoExtension.class)
+public @interface UnitTest {
+
+}
