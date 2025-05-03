c015a98effa780bfb6f1108aab9c165e4bc58850
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-05-04 00:32:47 +0900

    refactor: aspect-ragtio 제거 및 width 100vw설정
    - margin-top설정해 헤더 들어갈 부분 확보

--- frontend/src/pages/IntroducePage/IntroducePage.styles.ts
+++ frontend/src/pages/IntroducePage/IntroducePage.styles.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { HeaderStyles } from '@/components/common/Header/Header.styles';
+
+export const CustomHeader = styled(HeaderStyles)`
+  background-color: transparent;
+`;
 
 export const IntroduceImage = styled.img`
-  width: 100%;
-  aspect-ratio: 1/5;
+  width: 100vw;
   height: auto;
   object-fit: cover;
+  margin-top: 62px;
 `;
