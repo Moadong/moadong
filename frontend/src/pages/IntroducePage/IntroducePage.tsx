291d45f5c97bea39bdf279d2e53b7f71e88de9d5
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-05-04 00:32:55 +0900

    feat: 소개페이지 헤더 추가

--- frontend/src/pages/IntroducePage/IntroducePage.tsx
+++ frontend/src/pages/IntroducePage/IntroducePage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import * as Styled from './IntroducePage.styles';
+import Header from '@/components/common/Header/Header';
 import IntroduceImage from '@/assets/images/Introduce.png';
 
 const IntroducePage = () => {
-  return <Styled.IntroduceImage src={IntroduceImage} alt='소개 이미지' />;
+  return (
+    <>
+      <Header />
+      <Styled.IntroduceImage src={IntroduceImage} alt='소개 이미지' />
+    </>
+  );
 };
 
 export default IntroducePage;
