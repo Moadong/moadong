7e2b62f7f986aecffaa04c17b317689097546679
Junseo Kim <semiwest2@gmail.com>
2025-05-09 16:58:00 +0900

    refactor: 활동사니 모달 내 useEffect 구조를 정리

--- frontend/src/pages/ClubDetailPage/components/PhotoList/PhotoModal/PhotoModal.tsx
+++ frontend/src/pages/ClubDetailPage/components/PhotoList/PhotoModal/PhotoModal.tsx
@@ -26,8 +26,6 @@ const PhotoModal = ({ isOpen, onClose, clubName, photos }: PhotoModalProps) => {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
     }
     return () => {
       document.body.style.overflow = '';
