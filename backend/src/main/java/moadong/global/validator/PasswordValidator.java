65e64b55ee8fc7534f56896ad24e4728f6979fa0
Zepelown <dbswjdqhd12@gmail.com>
2025-05-10 21:37:59 +0900

    chore: Validator 주석 추가

--- backend/src/main/java/moadong/global/validator/PasswordValidator.java
+++ backend/src/main/java/moadong/global/validator/PasswordValidator.java
@@ -7,6 +7,7 @@ import moadong.global.annotation.Password;
 import java.util.regex.Pattern;
 
 public class PasswordValidator implements ConstraintValidator<Password, String> {
+    // 8 ~ 20자이고, 반드시 한 개의 알파벳, 한 개의 숫자, !@#$%^ 중 한 개가 모두 들어 갔는지 확인
     private static final Pattern PASSWORD_PATTERN = Pattern.compile("\"^(?=.*[a-zA-Z])(?=.*\\\\d)(?=.*[!@#$%^])[a-zA-Z\\\\d!@#$%^]{8,20}$\"\n");
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
