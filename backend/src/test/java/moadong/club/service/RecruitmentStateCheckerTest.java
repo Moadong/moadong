e13ff04fc3b856a0d24f959e714230e07e7e5d31
FhRh <yiyop@naver.com>
2025-05-13 17:13:49 +0900

    test : 모집상태 ALWAYS이면 변경 발생하지 않음

---
+++ backend/src/test/java/moadong/club/service/RecruitmentStateCheckerTest.java
@@ -0,0 +1,48 @@
+package moadong.club.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import moadong.club.entity.Club;
+import moadong.club.entity.ClubRecruitmentInformation;
+import moadong.club.enums.ClubRecruitmentStatus;
+import moadong.club.repository.ClubRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RecruitmentStateCheckerTest {
+
+    @InjectMocks
+    private RecruitmentStateChecker recruitmentStateChecker;
+
+    @Mock
+    private ClubRepository clubRepository;
+
+    static final ZonedDateTime NOW = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+
+    @Test
+    void 모집상태_ALWAYS_이면_변경되지_않음() {
+        Club club = mock(Club.class);
+        ClubRecruitmentInformation info = mock(ClubRecruitmentInformation.class);
+
+        when(club.getClubRecruitmentInformation()).thenReturn(info);
+        when(info.getClubRecruitmentStatus()).thenReturn(ClubRecruitmentStatus.ALWAYS);
+
+        when(clubRepository.findAll()).thenReturn(List.of(club));
+
+        recruitmentStateChecker.performTask();
+
+        verify(club, never()).updateRecruitmentStatus(any());
+        verify(clubRepository, never()).save(club);
+    }
+}
