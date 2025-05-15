9825ca52cbda44261f4bc5f540f58f03a24155c0
Junseo Kim <semiwest2@gmail.com>
2025-05-15 21:21:30 +0900

    chore: ESLint 설정에 TypeScript 및 Prettier 지원 추가

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,14 +1,59 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import typescript from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import storybook from 'eslint-plugin-storybook';
+import pluginPrettier from 'eslint-plugin-prettier';
 
+const config = {
+  ignores: [
+    'dist/**',
+    'node_modules/**',
+    'coverage/**',
+    'build/**',
+    'public/**',
+    '.eslintrc.js',
+    'jest.config.js',
+    'jest.setup.ts',
+    'netlify.toml',
+    'tsconfig.json',
+  ],
+  languageOptions: {
+    parser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: './tsconfig.json',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    globals: {
+      React: 'writable',
+    },
+  },
+  plugins: {
+    '@typescript-eslint': typescript,
+    react,
+    storybook,
+    prettier: pluginPrettier,
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-console': 'warn',
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
+
+export default config;
