e807524703d7153396591f39a1f63eb20e93901a
lepitaaar <leekus1005@gmail.com>
2025-05-17 14:26:59 +0900

    refactor: update와 create 미리 분리

--- backend/src/main/java/moadong/club/service/ClubApplyService.java
+++ backend/src/main/java/moadong/club/service/ClubApplyService.java
@@ -2,7 +2,8 @@ package moadong.club.service;
 
 import lombok.AllArgsConstructor;
 import moadong.club.entity.*;
-import moadong.club.payload.request.ClubApplicationRequest;
+import moadong.club.payload.request.ClubApplicationCreateRequest;
+import moadong.club.payload.request.ClubApplicationEditRequest;
 import moadong.club.payload.request.ClubApplyRequest;
 import moadong.club.repository.ClubApplicationRepository;
 import moadong.club.repository.ClubQuestionRepository;
@@ -23,29 +24,28 @@ public class ClubApplyService {
     private final ClubQuestionRepository clubQuestionRepository;
     private final ClubApplicationRepository clubApplicationRepository;
 
-    public void createClubApplication(String clubId, CustomUserDetails user, ClubApplicationRequest request) {
+    public void createClubApplication(String clubId, CustomUserDetails user, ClubApplicationCreateRequest request) {
         ClubQuestion clubQuestion = validateClubApplicationRequest(clubId, user);
 
-        clubQuestionRepository.save(updateQuestions(clubQuestion, request));
+        clubQuestionRepository.save(createQuestions(clubQuestion, request));
     }
 
-    public void editClubApplication(String clubId, CustomUserDetails user, ClubApplicationRequest request) {
+    public void editClubApplication(String clubId, CustomUserDetails user, ClubApplicationEditRequest request) {
         ClubQuestion clubQuestion = validateClubApplicationRequest(clubId, user);
 
         clubQuestionRepository.save(updateQuestions(clubQuestion, request));
     }
 
     public ResponseEntity<?> getClubApplication(String clubId) {
-        ClubQuestion clubQuestion = clubQuestionRepository.findByClubId(clubId).orElse(null);
-        if (clubQuestion == null) throw new RestApiException(ErrorCode.APPLICATION_NOT_FOUND);
+        ClubQuestion clubQuestion = clubQuestionRepository.findByClubId(clubId)
+                .orElseThrow(() -> new RestApiException(ErrorCode.APPLICATION_NOT_FOUND));
 
         return ResponseEntity.ok(clubQuestion);
     }
 
     public ResponseEntity<?> applyToClub(String clubId, ClubApplyRequest request) {
-        //getCLubApplication과 중복된 코드 -> 분리??
-        ClubQuestion clubQuestion = clubQuestionRepository.findByClubId(clubId).orElse(null);
-        if (clubQuestion == null) throw new RestApiException(ErrorCode.APPLICATION_NOT_FOUND);
+        ClubQuestion clubQuestion = clubQuestionRepository.findByClubId(clubId)
+                .orElseThrow(() -> new RestApiException(ErrorCode.APPLICATION_NOT_FOUND));
 
         //질문자의 응답에대해 따로 검증 진행하지 않음, 프론트에서 제한하는것만으로도 충분하다 생각
         List<ClubQuestionAnswer> answers = request.questions()
@@ -64,7 +64,7 @@ public class ClubApplyService {
         return ResponseEntity.ok("success apply");
     }
 
-    private ClubQuestion updateQuestions(ClubQuestion clubQuestion, ClubApplicationRequest request) {
+    private ClubQuestion createQuestions(ClubQuestion clubQuestion, ClubApplicationCreateRequest request) {
         //지금은 지원서 하나만
         List<ClubApplicationQuestion> newQuestions = request.questions().stream()
                 .map(question -> ClubApplicationQuestion.builder()
@@ -72,8 +72,45 @@ public class ClubApplyService {
                         .title(question.title())
                         .description(question.description())
                         .type(question.type())
-                        .options(question.options())
-                        .items(question.items())
+                        .options(
+                                ClubQuestionOption.builder()
+                                        .required(question.options().required())
+                                        .build()
+                        )
+                        .items(question.items().stream()
+                                .map(item ->
+                                        ClubQuestionItem.builder()
+                                                .value(item.value())
+                                                .build())
+                                .toList())
+                        .build())
+                .toList();
+
+        clubQuestion.updateQuestions(newQuestions);
+        clubQuestion.updateFormTitle(request.title());
+
+        return clubQuestion;
+    }
+
+    private ClubQuestion updateQuestions(ClubQuestion clubQuestion, ClubApplicationEditRequest request) {
+        //지금은 지원서 하나만
+        List<ClubApplicationQuestion> newQuestions = request.questions().stream()
+                .map(question -> ClubApplicationQuestion.builder()
+                        .id(question.id())
+                        .title(question.title())
+                        .description(question.description())
+                        .type(question.type())
+                        .options(
+                                ClubQuestionOption.builder()
+                                        .required(question.options().required())
+                                        .build()
+                        )
+                        .items(question.items().stream()
+                                .map(item ->
+                                        ClubQuestionItem.builder()
+                                                .value(item.value())
+                                                .build())
+                                .toList())
                         .build())
                 .toList();
 
