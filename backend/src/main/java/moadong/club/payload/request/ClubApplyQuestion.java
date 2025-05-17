1c4755d2ec404f1b60fe36cfbce25a32f4c3aa02
lepitaaar <leekus1005@gmail.com>
2025-05-17 14:27:09 +0900

    refactor: ClubApplyQuestion 분할

---
+++ backend/src/main/java/moadong/club/payload/request/ClubApplyQuestion.java
@@ -0,0 +1,32 @@
+package moadong.club.payload.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import moadong.club.enums.ClubApplicationQuestionType;
+
+import java.util.List;
+
+public record ClubApplyQuestion(
+        @NotNull
+        Long id,
+        @NotBlank
+        String title,
+        @NotNull //빈칸 허용
+        String description,
+        @NotNull
+        ClubApplicationQuestionType type,
+        @NotNull
+        Options options,
+        @NotNull
+        List<QuestionItem> items
+) {
+    public record Options(
+            @NotNull
+            boolean required
+    ) {}
+
+    public record QuestionItem(
+            String value
+    ) {}
+}
