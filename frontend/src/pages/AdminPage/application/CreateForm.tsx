c5d7b7c9972e25dc612202a7be5a26234d585d3b
Junseo Kim <semiwest2@gmail.com>
2025-05-26 13:17:57 +0900

    feat: 지원서 생성 컴포넌트 CreateForm 구현
    질문 데이터를 상태로 관리하며, QuestionBuilder 컴포넌트를 통해 질문 편집 UI를 구성했습니다.
    질문 제목, 설명, 항목, 질문 타입 변경을 위한 핸들러를 각각 구현했습니다.
    
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

---
+++ frontend/src/pages/AdminPage/application/CreateForm.tsx
@@ -0,0 +1,102 @@
+// 지원서 제작하기 : 지원서 제작 컴포넌트
+import { useState } from 'react';
+import QuestionBuilder from '@/pages/AdminPage/application/components/QuestionBuilder/QuestionBuilder';
+
+type QuestionType =
+  | 'SHORT_TEXT'
+  | 'LONG_TEXT'
+  | 'CHOICE'
+  | 'MULTI_CHOICE'
+  | 'EMAIL'
+  | 'PHONE_NUMBER'
+  | 'NAME';
+
+type Question = {
+  title: string;
+  description: string;
+  items?: { value: string }[];
+  type: QuestionType;
+};
+
+const CreateForm = () => {
+  const [questions, setQuestions] = useState<Record<number, Question>>({
+    1: {
+      title: '이름을 입력해주세요',
+      description: '본명을 입력해 주세요',
+      type: 'SHORT_TEXT',
+      items: [],
+    },
+    2: {
+      title: '지원 분야를 선택해주세요',
+      description: '중복 선택은 불가능합니다',
+      items: [
+        { value: '프론트엔드' },
+        { value: '백엔드' },
+        { value: '디자인' },
+      ],
+      type: 'MULTI_CHOICE',
+    },
+  });
+
+  const handleTitleChange = (id: number) => (value: string) => {
+    setQuestions((prev) => ({
+      ...prev,
+      [id]: {
+        ...prev[id],
+        title: value,
+      },
+    }));
+  };
+
+  const handleDescriptionChange = (id: number) => (value: string) => {
+    setQuestions((prev) => ({
+      ...prev,
+      [id]: {
+        ...prev[id],
+        description: value,
+      },
+    }));
+  };
+
+  const handleItemsChange = (id: number) => (newItems: { value: string }[]) => {
+    setQuestions((prev) => ({
+      ...prev,
+      [id]: {
+        ...prev[id],
+        items: newItems,
+      },
+    }));
+  };
+
+  const handleTypeChange = (id: number) => (newType: QuestionType) => {
+    setQuestions((prev) => ({
+      ...prev,
+      [id]: {
+        ...prev[id],
+        type: newType,
+      },
+    }));
+  };
+
+  return (
+    <>
+      {Object.entries(questions).map(([id, question]) => (
+        <QuestionBuilder
+          key={id}
+          id={Number(id)}
+          title={question.title}
+          description={question.description}
+          required={true}
+          options={question.items}
+          onTitleChange={handleTitleChange(Number(id))}
+          onDescriptionChange={handleDescriptionChange(Number(id))}
+          onItemsChange={handleItemsChange(Number(id))}
+          onTypeChange={handleTypeChange(Number(id))}
+          type={question.type}
+        />
+      ))}
+    </>
+  );
+};
+
+export default CreateForm;
