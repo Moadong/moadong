d0c9b4d19666691aacf853db1280d7d36cc4f3d5
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-05-26 10:01:11 +0900

    feat: 에러 케이스 추가
    - 날짜 부분 누락된 경우
    - 빈 문자열인 경우

--- frontend/src/utils/stringToDate.test.ts
+++ frontend/src/utils/stringToDate.test.ts
@@ -13,15 +13,29 @@ describe('stringToDate 함수 테스트', () => {
     expect(stringToDate(input)).toEqual(result);
   });
 
-  it('공백이 없는 문자열이 주어지면 예외를 발생시킨다.', () => {
+  it('공백이 없는 문자열이라면 예외가 발생한다.', () => {
     const input = '2025.05.2513:45';
     expect(() => stringToDate(input)).toThrow(
       '유효하지 않은 날짜 형식입니다. 형식은 "YYYY.MM.DD HH:mm" 이어야 합니다.',
     );
   });
 
-  it('시간 형식이 누락된 경우 예외가 발생한다', () => {
+  it('시간 부분이 누락된 경우 예외가 발생한다', () => {
     const input = '2025.05.25 ';
+    expect(() => stringToDate(input)).toThrow(
+      '유효하지 않은 날짜 형식입니다. 형식은 "YYYY.MM.DD HH:mm" 이어야 합니다.',
+    );
+  });
+
+  it('날짜 부분이 누락된 경우 예외가 발생한다', () => {
+    const input = ' 13:45';
+    expect(() => stringToDate(input)).toThrow(
+      '유효하지 않은 날짜 형식입니다. 형식은 "YYYY.MM.DD HH:mm" 이어야 합니다.',
+    );
+  });
+
+  it('빈 문자열이 주어지면 예외가 발생한다', () => {
+    const input = '';
     expect(() => stringToDate(input)).toThrow(
       '유효하지 않은 날짜 형식입니다. 형식은 "YYYY.MM.DD HH:mm" 이어야 합니다.',
     );
