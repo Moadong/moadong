a6fae149bf6ee29e2a5cb1fa2e503dedc73a4c53
Junseo Kim <semiwest2@gmail.com>
2025-05-26 21:30:35 +0900

    fix: LONG_TEXT 질문 타입 최대 500자로 placeholder를 수정했습니다.

--- frontend/src/constants/APPLICATION_FORM.ts
+++ frontend/src/constants/APPLICATION_FORM.ts
@@ -3,7 +3,7 @@ const APPLICATION_FORM = {
     placeholder: '답변입력란(최대 20자)',
   },
   LONG_TEXT: {
-    placeholder: '답변입력란(최대 20자)',
+    placeholder: '답변입력란(최대 500자)',
   },
   CHOICE: {
     placeholder: '항목(최대 20자)',
