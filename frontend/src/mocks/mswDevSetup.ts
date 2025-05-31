8b376021b46d9a5a876ee525dba51c720d7adedc
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-05-31 23:06:20 +0900

    refactor: mswDevSetup 앞의 의문의 숫자 제거
    Co-Authored-By: 김준서 Junseo Kim <semiwest2@gmail.com>

--- frontend/src/mocks/mswDevSetup.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './api';
-import { RequestHandler } from 'msw/lib/core/handlers/RequestHandler.mjs';
-
-export const worker = setupWorker(...(handlers as unknown as RequestHandler[]));
