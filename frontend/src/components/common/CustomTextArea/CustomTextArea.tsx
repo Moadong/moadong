1876224d04d71089b32796831ecefd46f402a1a1
Junseo Kim <semiwest2@gmail.com>
2025-06-01 00:11:46 +0900

    feat: CustomTextArea 컴포넌트 추가
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

---
+++ frontend/src/components/common/CustomTextArea/CustomTextArea.tsx
@@ -0,0 +1,63 @@
+import * as Styled from './CustomTextArea.styles';
+
+interface CustomInputProps {
+  placeholder?: string;
+  width?: string;
+  maxLength?: number;
+  label?: string;
+  showMaxChar?: boolean;
+  disabled?: boolean;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onClear?: () => void;
+  isError?: boolean;
+  helperText?: string;
+}
+
+const CustomTextArea = ({
+  placeholder = '입력하세요',
+  width = '100%',
+  maxLength,
+  label,
+  showMaxChar = false,
+  disabled = false,
+  value = '',
+  onChange,
+  isError,
+  helperText,
+}: CustomInputProps) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+    if (maxLength !== undefined && inputValue.length > maxLength) {
+      return;
+    }
+    onChange?.(e);
+  };
+
+  return (
+    <Styled.InputContainer width={width}>
+      {label && <Styled.Label>{label}</Styled.Label>}
+      <Styled.InputWrapper>
+        <Styled.TextArea
+          onChange={handleChange}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          disabled={disabled}
+          hasError={isError}
+        >
+          {value}
+        </Styled.TextArea>
+        {showMaxChar && maxLength !== undefined && (
+          <Styled.CharCount>
+            {value.length}/{maxLength}
+          </Styled.CharCount>
+        )}
+      </Styled.InputWrapper>
+      {isError && helperText && (
+        <Styled.HelperText>{helperText}</Styled.HelperText>
+      )}
+    </Styled.InputContainer>
+  );
+};
+
+export default CustomTextArea;
