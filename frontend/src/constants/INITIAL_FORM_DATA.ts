98d28927fc56118f4a7c02daf985388f7ee1639c
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-05-31 19:08:05 +0900

    feat: 지원서 최초 생성 시 템플릿 추가
    Co-Authored-By: 김준서 Junseo Kim <semiwest2@gmail.com>

---
+++ frontend/src/constants/INITIAL_FORM_DATA.ts
@@ -0,0 +1,24 @@
+import { ApplicationFormData } from '@/types/application';
+
+const INITIAL_FORM_DATA: ApplicationFormData = {
+  form_title: '',
+  questions: [
+    {
+      id: 1,
+      title: '',
+      description: '',
+      type: 'SHORT_TEXT',
+      options: { required: true },
+    },
+    {
+      id: 2,
+      title: '',
+      description: '',
+      type: 'CHOICE',
+      options: { required: true },
+      items: [{ value: '' }, { value: '' }],
+    },
+  ],
+};
+
+export default INITIAL_FORM_DATA;
