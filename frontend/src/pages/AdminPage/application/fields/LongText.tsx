ed619d3a09256d2a2412caa41c18e00d405fc89a
Junseo Kim <semiwest2@gmail.com>
2025-06-01 00:12:24 +0900

    feat: LongText 컴포넌트 추가
    CustomTextArea 컴포넌트를 활용해 텍스트 입력 필드를 구현
    
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

---
+++ frontend/src/pages/AdminPage/application/fields/LongText.tsx
@@ -0,0 +1,46 @@
+import QuestionTitle from '@/pages/AdminPage/application/components/QuestionTitle/QuestionTitle';
+import QuestionDescription from '@/pages/AdminPage/application/components/QuestionDescription/QuestionDescription';
+import APPLICATION_FORM from '@/constants/APPLICATION_FORM';
+import { TextProps } from '@/types/application';
+import CustomTextArea from '@/components/common/CustomTextArea/CustomTextArea';
+
+const LongText = ({
+  id,
+  title,
+  description,
+  required,
+  answer,
+  mode,
+  onAnswerChange,
+  onTitleChange,
+  onDescriptionChange,
+}: TextProps) => {
+  return (
+    <div>
+      <QuestionTitle
+        id={id}
+        title={title}
+        required={required}
+        mode={mode}
+        onTitleChange={onTitleChange}
+      />
+      <QuestionDescription
+        description={description}
+        mode={mode}
+        onDescriptionChange={onDescriptionChange}
+      />
+      <CustomTextArea
+        value={answer}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+          onAnswerChange?.(e.target.value)
+        }
+        placeholder={APPLICATION_FORM.LONG_TEXT.placeholder}
+        disabled={mode === 'builder'}
+        showMaxChar={mode === 'answer'}
+        maxLength={APPLICATION_FORM.LONG_TEXT.maxLength}
+      />
+    </div>
+  );
+};
+
+export default LongText;
