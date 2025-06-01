69e2df0bbf65e81da711d18d6566e4460c279f89
Zepelown <dbswjdqhd12@gmail.com>
2025-06-02 03:04:36 +0900

    fix: 잘못된 비밀번호 검증 로직과 잘못된 비밀번호 테스트 수정

--- backend/src/main/java/moadong/global/validator/PasswordValidator.java
+++ backend/src/main/java/moadong/global/validator/PasswordValidator.java
@@ -7,13 +7,16 @@ import moadong.global.annotation.Password;
 import java.util.regex.Pattern;
 
 public class PasswordValidator implements ConstraintValidator<Password, String> {
-    // 8 ~ 20자이고, 반드시 한 개의 알파벳, 한 개의 숫자, !@#$%^ 중 한 개가 모두 들어 갔는지 확인
-    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^]).{8,20}$");
+    private static final String REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^])(?!.*\\s).{8,20}$";
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(REGEX);
+
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(String s, ConstraintValidatorContext context) {
         if (s == null || s.isEmpty()) {
-            return true;
+            return false;
         }
         return PASSWORD_PATTERN.matcher(s).matches();
     }
+
 }
+
