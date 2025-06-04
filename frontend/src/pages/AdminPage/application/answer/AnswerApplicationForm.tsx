f706635901a999216ed1d29a5547a71985a1ff57
Junseo Kim <semiwest2@gmail.com>
2025-06-04 15:20:31 +0900

    feat: AnswerApplicationForm 컴포넌트 추가
    1. 동아리 지원서 답변 화면 구현을 위한 AnswerApplicationForm 컴포넌트 신규 추가
    2. ClubProfile, QuestionAnswerer, mockData 등을 활용한 화면 구성
    3. 하드코딩된 clubId 사용 및 기본 예외 처리 추가

---
+++ frontend/src/pages/AdminPage/application/answer/AnswerApplicationForm.tsx
@@ -0,0 +1,52 @@
+import { mockData } from '@/mocks/data/mockData';
+import { PageContainer } from '@/styles/PageContainer.styles';
+import * as Styled from './AnswerApplicationForm.styles';
+import Header from '@/components/common/Header/Header';
+import { useParams } from 'react-router-dom';
+import { useGetClubDetail } from '@/hooks/queries/club/useGetClubDetail';
+import ClubProfile from '@/pages/ClubDetailPage/components/ClubProfile/ClubProfile';
+import { useAnswers } from '@/hooks/useAnswers';
+import QuestionAnswerer from '@/pages/AdminPage/application/components/QuestionAnswerer/QuestionAnswerer';
+
+const AnswerApplicationForm = () => {
+  //const { clubId } = useParams<{ clubId: string }>();
+  const clubId = '67e54ae51cfd27718dd40bec'; // 하드코딩된 club ID
+  const { data: clubDetail, error } = useGetClubDetail(clubId || '');
+  const { onAnswerChange, getAnswersById } = useAnswers();
+  if (!clubDetail) {
+    return null;
+  }
+
+  if (error) {
+    return <div>에러가 발생했습니다.</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <PageContainer style={{ paddingTop: '172px' }}>
+        <ClubProfile
+          name={clubDetail.name}
+          logo={clubDetail.logo}
+          division={clubDetail.division}
+          category={clubDetail.category}
+          tags={clubDetail.tags}
+        />
+        <Styled.FormTitle>{mockData.form_title}</Styled.FormTitle>
+        {mockData.questions.map((q) => (
+          <QuestionAnswerer
+            key={q.id}
+            question={q}
+            selectedAnswers={getAnswersById(q.id)}
+            onChange={onAnswerChange}
+          />
+        ))}
+        <Styled.ButtonWrapper>
+          <Styled.submitButton>제출하기</Styled.submitButton>
+        </Styled.ButtonWrapper>
+      </PageContainer>
+    </>
+  );
+};
+
+export default AnswerApplicationForm;
