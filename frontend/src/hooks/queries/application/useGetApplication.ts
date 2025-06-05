00a8e3374f38827eb46d345e1aebcbaa90333483
Junseo Kim <semiwest2@gmail.com>
2025-06-05 15:48:47 +0900

    feat: 클럽 지원서 조회 리액트 쿼리 훅 추가
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

---
+++ frontend/src/hooks/queries/application/useGetApplication.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import getApplication from '@/apis/application/getApplication';
+
+export const useGetApplication = (clubId: string) => {
+  return useQuery({
+    queryKey: ['applicationForm', clubId],
+    queryFn: () => getApplication(clubId),
+    retry: false,
+  });
+};
