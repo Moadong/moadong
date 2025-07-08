d19c125f3e358c3238418696a13ef1fdc791a159
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-07-08 12:08:40 +0900

    feat: ClubApplicationRepository에 findAllByQuestionId 메서드 추가

--- backend/src/main/java/moadong/club/repository/ClubApplicationRepository.java
+++ backend/src/main/java/moadong/club/repository/ClubApplicationRepository.java
@@ -3,5 +3,9 @@ package moadong.club.repository;
 import moadong.club.entity.ClubApplication;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ClubApplicationRepository extends MongoRepository<ClubApplication, String> {
+    
+    List<ClubApplication> findAllByQuestionId(String questionId);
 }
