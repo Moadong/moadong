8b5334ee70006acbb0a2650467d51a22e5e8c247
lepitaaar <leekus1005@gmail.com>
2025-07-23 12:23:06 +0900

    fix: cookie에 도메인설정

--- backend/src/main/java/moadong/user/util/CookieMaker.java
+++ backend/src/main/java/moadong/user/util/CookieMaker.java
@@ -9,12 +9,17 @@ public class CookieMaker {
     @Value("${jwt.refresh.token.expiration.hour}")
     private int refreshTokenExpirationHour;
 
+    @Value("${server.domain}")
+    private String serverDomain;
+
     public ResponseCookie makeRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refresh_token", refreshToken)
                 .httpOnly(true)
                 .path("/")
                 .maxAge((long) refreshTokenExpirationHour * 60 * 60)
-                .secure(true)
+                //safari에선 localhost에 쿠키를 저장하기위해선 secure를 꺼줘야 저장이된다
+                .secure(!serverDomain.contains("localhost"))
+                .domain(".moadong.com")
                 .sameSite("None")
                 .build();
     }
