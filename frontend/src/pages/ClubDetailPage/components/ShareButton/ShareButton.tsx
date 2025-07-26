a8be6468f7a6a7d6adee029297609d0014a7afb5
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-07-27 00:51:16 +0900

    refactor: 카카오톡 공유하기 커스텀 버튼으로 변경
    - createDefaultButton 방식에서 커스텀 버튼(onClick + sendDefault) 방식으로 변경
    - 버튼 클릭 시 카카오 공유와 믹스패널 트래킹이 정확히 동작하도록 개선

--- frontend/src/pages/ClubDetailPage/components/ShareButton/ShareButton.tsx
+++ frontend/src/pages/ClubDetailPage/components/ShareButton/ShareButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import * as Styled from './ShareButton.styles';
 import ShareIcon from '@/assets/images/icons/share_icon.svg';
 import { useGetClubDetail } from '@/hooks/queries/club/useGetClubDetail';
@@ -9,53 +9,43 @@ interface ShareButtonProps {
 }
 
 const ShareButton = ({ clubId }: ShareButtonProps) => {
-  const kakaoBtnRef = useRef<HTMLDivElement>(null);
-  const { data: clubDetail } = useGetClubDetail(clubId as string);
+  const { data: clubDetail } = useGetClubDetail(clubId);
   const trackEvent = useMixpanelTrack();
 
-  useEffect(() => {
+  const handleShare = () => {
     if (!window.Kakao || !window.Kakao.isInitialized()) {
       alert('카카오 SDK가 아직 준비되지 않았습니다.');
       return;
     }
-
     if (!clubDetail) return;
-
-    if (kakaoBtnRef.current && kakaoBtnRef.current.childElementCount === 0) {
-      window.Kakao.Share.createDefaultButton({
-        container: kakaoBtnRef.current,
-        objectType: 'feed',
-        content: {
-          title: clubDetail.name,
-          description: clubDetail.description,
-          imageUrl: clubDetail.logo
-            ? clubDetail.logo
-            : 'https://avatars.githubusercontent.com/u/200371900?s=200&v=4',
+    window.Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: clubDetail.name,
+        description: clubDetail.description,
+        imageUrl: clubDetail.logo
+          ? clubDetail.logo
+          : 'https://avatars.githubusercontent.com/u/200371900?s=200&v=4',
+        link: {
+          mobileWebUrl: `https://www.moadong.com/club/${clubDetail.id}`,
+          webUrl: `https://www.moadong.com/club/${clubDetail.id}`,
+        },
+      },
+      buttons: [
+        {
+          title: '모아동에서 지원하기',
           link: {
             mobileWebUrl: `https://www.moadong.com/club/${clubDetail.id}`,
             webUrl: `https://www.moadong.com/club/${clubDetail.id}`,
           },
         },
-        buttons: [
-          {
-            title: '모아동에서 지원하기',
-            link: {
-              mobileWebUrl: `https://www.moadong.com/club/${clubDetail.id}`,
-              webUrl: `https://www.moadong.com/club/${clubDetail.id}`,
-            },
-          },
-        ],
-      });
-    }
-  }, [clubId, clubDetail]);
+      ],
+    });
+    trackEvent(`${clubDetail.name} 공유하기 버튼 클릭`);
+  };
 
   return (
-    <Styled.ShareButtonContainer
-      ref={kakaoBtnRef}
-      onClick={() => {
-        trackEvent(`${clubDetail?.name} 공유하기 버튼 클릭`);
-      }}
-    >
+    <Styled.ShareButtonContainer onClick={handleShare}>
       <img src={ShareIcon} alt='카카오톡 공유' />
     </Styled.ShareButtonContainer>
   );
