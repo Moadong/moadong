483c14fafd0d9f1bdcee9c62f6c484f2acd9d620
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-08-13 16:54:32 +0900

    feat: 실시간 검색을 위해 autoBlur false로 변경

--- frontend/src/pages/AdminPage/tabs/ApplicantsTab/ApplicantsTab.tsx
+++ frontend/src/pages/AdminPage/tabs/ApplicantsTab/ApplicantsTab.tsx
@@ -98,6 +98,7 @@ const ApplicantsTab = () => {
             value={keyword}
             onChange={setKeyword}
             onSubmit={() => {}}
+            autoBlur={false}
             placeholder='지원자 이름을 입력해주세요'
             ariaLabel='지원자 검색창'
           />
