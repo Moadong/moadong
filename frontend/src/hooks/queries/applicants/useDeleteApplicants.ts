1a84b37bd0bc8bb76f216e4a25625bda28345b36
lepitaaar <leekus1005@gmail.com>
2025-08-24 18:15:42 +0900

    feat: 지원자 삭제 훅 추가

---
+++ frontend/src/hooks/queries/applicants/useDeleteApplicants.ts
@@ -0,0 +1,17 @@
+import deleteApplicants from '@/apis/applicants/deleteApplicants';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useDeleteApplicants = (clubId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ applicantIds }: { applicantIds: string[] }) =>
+      deleteApplicants(applicantIds, clubId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['clubApplicants'] });
+    },
+    onError: (error) => {
+      console.log(`Error delete applicants detail: ${error}`);
+    },
+  });
+};
