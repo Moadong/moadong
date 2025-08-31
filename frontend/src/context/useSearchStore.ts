6c76c642f9141184bedc70b9c939c099b2e63ca2
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-09-01 01:39:00 +0900

    refactor: 검색 상태 관리를 Zustand로 변경하여 성능 최적화
    - React Context에서 Zustand로 마이그레이션
    - 검색창 입력 시 헤더와 카테고리 리스트 리렌더링 방지
    - 개별 상태 구독을 통한 컴포넌트 최적화

---
+++ frontend/src/context/useSearchStore.ts
@@ -0,0 +1,63 @@
+import { create } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
+
+interface SearchStore {
+  keyword: string;
+  setKeyword: (keyword: string) => void;
+  inputValue: string;
+  setInputValue: (value: string) => void;
+  isSearching: boolean;
+  setIsSearching: (isSearching: boolean) => void;
+}
+
+export const useSearchStore = create<SearchStore>()(
+  subscribeWithSelector((set) => ({
+    keyword: '',
+    setKeyword: (keyword) => set({ keyword }),
+    inputValue: '',
+    setInputValue: (value) => set({ inputValue: value }),
+    isSearching: false,
+    setIsSearching: (isSearching) => set({ isSearching }),
+  })),
+);
+
+export const useSearch = () => {
+  const {
+    keyword,
+    setKeyword,
+    inputValue,
+    setInputValue,
+    isSearching,
+    setIsSearching,
+  } = useSearchStore();
+
+  return {
+    keyword,
+    setKeyword,
+    inputValue,
+    setInputValue,
+    isSearching,
+    setIsSearching,
+  };
+};
+
+export const useSearchKeyword = () => {
+  const keyword = useSearchStore((state) => state.keyword);
+  const setKeyword = useSearchStore((state) => state.setKeyword);
+  return { keyword, setKeyword };
+};
+
+export const useSearchIsSearching = () => {
+  const isSearching = useSearchStore((state) => state.isSearching);
+  const setIsSearching = useSearchStore((state) => state.setIsSearching);
+  return { isSearching, setIsSearching };
+};
+
+export const useSearchInput = () => {
+  const inputValue = useSearchStore((state) => state.inputValue);
+  const setInputValue = useSearchStore((state) => state.setInputValue);
+  const setKeyword = useSearchStore((state) => state.setKeyword);
+  const setIsSearching = useSearchStore((state) => state.setIsSearching);
+
+  return { inputValue, setInputValue, setKeyword, setIsSearching };
+};
