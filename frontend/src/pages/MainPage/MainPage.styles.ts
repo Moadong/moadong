d5c87952e62adbfc3d0b216bf56488fbbfd58837
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-09-22 21:10:11 +0900

    style: 탭 선택 밑줄 모서리 디자인 변경

--- frontend/src/pages/MainPage/MainPage.styles.ts
+++ frontend/src/pages/MainPage/MainPage.styles.ts
@@ -47,6 +47,7 @@ export const Tab = styled.button<{$active?: boolean}>`
     width: 100%;
     height: 1.5px;
     background: #787878;
+    border-radius: 1.5px;
     transform: ${({$active}) => $active ? 'scaleX(1)' : 'scaleX(0)'};
     transform-origin: center;
     transition: transform 0.2s ease;
