d987a5839b0ca325b918a253dcf17966ddd2d792
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-10-05 14:53:43 +0900

    refactor: 데스크탑 padding제거

--- frontend/src/pages/MainPage/MainPage.styles.ts
+++ frontend/src/pages/MainPage/MainPage.styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const PageContainer = styled.div`
-  padding: 0 40px;
   max-width: 1180px;
   margin: 0 auto;
+
+  @media (max-width: 1280px) {
+    padding: 0 20px;
+  }
 
   @media (max-width: 500px) {
     padding: 0 20px;
