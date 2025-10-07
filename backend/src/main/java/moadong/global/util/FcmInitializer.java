1d7fcc59164566b10c8f02e51a753ee73a2a66c9
lepitaaar <leekus1005@gmail.com>
2025-10-07 18:27:59 +0900

    refactor: fcm 초기화가 되지않을시 어플리케이션 실행 안되게 변경

--- backend/src/main/java/moadong/global/util/FcmInitializer.java
+++ backend/src/main/java/moadong/global/util/FcmInitializer.java
@@ -8,28 +8,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Slf4j
 @Component
 public class FcmInitializer {
-    private String fcmConfigPath = "";
 
     @PostConstruct
-    public void init() {
-        try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("resources/firebase.json");
+    public void init() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("./src/main/resources/firebase.json");
 
-            FirebaseOptions.Builder options = FirebaseOptions.builder();
-            options.setCredentials(GoogleCredentials.fromStream(serviceAccount));
+        FirebaseOptions.Builder options = FirebaseOptions.builder();
+        options.setCredentials(GoogleCredentials.fromStream(serviceAccount));
 
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options.build());
-                log.info("Firebase app has been initialized");
-            }
-        } catch (IOException e) {
-            log.error("Error initializing Firebase app", e);
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options.build());
+            log.info("Firebase app has been initialized");
         }
     }
 }
