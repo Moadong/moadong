110cce560452eded4bef0b48219ccb9bd42f0c4c
lepitaaar <leekus1005@gmail.com>
2025-10-10 22:25:02 +0900

    fix: classpath로 빌드 후 resource 파일 가져올 수 있게 수정

--- backend/src/main/java/moadong/global/util/FcmInitializer.java
+++ backend/src/main/java/moadong/global/util/FcmInitializer.java
@@ -5,10 +5,9 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Slf4j
@@ -17,11 +16,11 @@ public class FcmInitializer {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("./src/main/resources/firebase.json");
+        ClassPathResource serviceAccount =
+                new ClassPathResource("firebase.json");
 
         FirebaseOptions.Builder options = FirebaseOptions.builder();
-        options.setCredentials(GoogleCredentials.fromStream(serviceAccount));
+        options.setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()));
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options.build());
