446263d30e24e8f3dbc403765243cfa43841d28b
lepitaaar <leekus1005@gmail.com>
2025-10-11 20:13:35 +0900

    feature: 블로킹 로직을 Async로 이동
    ApiFuture.get()은 블로킹 메서드. 그렇기에 메인 쓰레드 대신 async 쓰레드에서 fcm subscription 오류 관리

---
+++ backend/src/main/java/moadong/fcm/service/FcmAsyncService.java
@@ -0,0 +1,66 @@
+package moadong.fcm.service;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.TopicManagementResponse;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import moadong.club.repository.ClubRepository;
+import moadong.fcm.entity.FcmToken;
+import moadong.fcm.repository.FcmTokenRepository;
+import moadong.global.exception.ErrorCode;
+import moadong.global.exception.RestApiException;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class FcmAsyncService {
+
+    private final FcmTokenRepository fcmTokenRepository;
+    private final ClubRepository clubRepository;
+
+    @Async
+    @Transactional
+    public void updateSubscriptions(String token, Set<String> newClubIds, Set<String> clubsToSubscribe, Set<String> clubsToUnsubscribe) {
+        List<ApiFuture<TopicManagementResponse>> futures = new ArrayList<>();
+
+        // 새로운 동아리 구독
+        if (!clubsToSubscribe.isEmpty()) {
+            for (String clubId : clubsToSubscribe) {
+                futures.add(FirebaseMessaging.getInstance().subscribeToTopicAsync(Collections.singletonList(token), clubId));
+            }
+        }
+
+        // 더 이상 구독하지 않는 동아리 구독 해제
+        if (!clubsToUnsubscribe.isEmpty()) {
+            for (String clubId : clubsToUnsubscribe) {
+                futures.add(FirebaseMessaging.getInstance().unsubscribeFromTopicAsync(Collections.singletonList(token), clubId));
+            }
+        }
+
+        try {
+            if (!futures.isEmpty()) {
+                List<TopicManagementResponse> responses = ApiFutures.allAsList(futures).get();
+
+                for (TopicManagementResponse response : responses) {
+                    if (response.getFailureCount() > 0) {
+                        log.error("Fcm topic subscription failed token {}. Response: {}", token, response.getErrors());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("error: {}", e.getMessage());
+            throw new RestApiException(ErrorCode.FCMTOKEN_SUBSCRIBE_ERROR);
+        }
+
+        FcmToken existToken = fcmTokenRepository.findFcmTokenByToken(token).get();
+        existToken.updateClubIds(newClubIds.stream().toList());
+        fcmTokenRepository.save(existToken);
+    }
+}
--- backend/src/main/java/moadong/fcm/service/FcmService.java
+++ backend/src/main/java/moadong/fcm/service/FcmService.java
@@ -1,6 +1,5 @@
 package moadong.fcm.service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import moadong.club.repository.ClubRepository;
@@ -11,16 +10,14 @@ import moadong.global.exception.ErrorCode;
 import moadong.global.exception.RestApiException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class FcmService {
     private final FcmTokenRepository fcmTokenRepository;
+    private final FcmAsyncService fcmAsyncService;
     private final ClubRepository clubRepository;
 
     public void saveFcmToken(String token) {
@@ -43,40 +40,24 @@ public class FcmService {
         FcmToken existToken = fcmTokenRepository.findFcmTokenByToken(token)
                 .orElseThrow(() -> new RestApiException(ErrorCode.FCMTOKEN_NOT_FOUND));
 
-        ArrayList<String> oldClubIds = existToken.getClubIds();
-
         Set<String> newClubIdSet = Set.copyOf(newClubIds);
-        Set<String> oldClubIdSet = Set.copyOf(oldClubIds);
+        Set<String> oldClubIdSet = Set.copyOf(existToken.getClubIds());
 
         Set<String> clubsToSubscribe = new HashSet<>(newClubIdSet);
         clubsToSubscribe.removeAll(oldClubIdSet);
 
         Set<String> clubsToUnsubscribe = new HashSet<>(oldClubIdSet);
         clubsToUnsubscribe.removeAll(newClubIdSet);
-        Long countClub = clubRepository.countByIdIn(clubsToSubscribe.stream().toList());
 
-        if (countClub != clubsToSubscribe.size()) {
-            throw new RestApiException(ErrorCode.CLUB_NOT_FOUND);
-        }
-
-        // 새로운 동아리 구독
-        if (!clubsToSubscribe.isEmpty()) {
-            for (String clubId : clubsToSubscribe) {
-                FirebaseMessaging.getInstance().subscribeToTopicAsync(Collections.singletonList(token), clubId);
-            }
-        }
-
-        // 더 이상 구독하지 않는 동아리 구독 해제
         if (!clubsToUnsubscribe.isEmpty()) {
-            for (String clubId : clubsToUnsubscribe) {
-                FirebaseMessaging.getInstance().unsubscribeFromTopicAsync(Collections.singletonList(token), clubId);
+            Long countClub = clubRepository.countByIdIn(clubsToSubscribe.stream().toList());
+
+            if (countClub != clubsToSubscribe.size()) {
+                throw new RestApiException(ErrorCode.CLUB_NOT_FOUND);
             }
         }
 
-        // DB에 최신 동아리 목록으로 업데이트
-        existToken.updateClubIds(newClubIds);
-        fcmTokenRepository.save(existToken);
-
+        fcmAsyncService.updateSubscriptions(token, newClubIdSet, clubsToSubscribe, clubsToUnsubscribe);
     }
 
     public ClubSubscribeListResponse getSubscribeClubs(String token) {
