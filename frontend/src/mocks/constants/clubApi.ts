8bba6d7309471029705c598899a784daa88dbf57
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-10-12 07:57:55 +0900

    refacter: mock 서버 상수 분리

--- frontend/src/mocks/constants/clubApi.ts
+++ frontend/src/mocks/constants/clubApi.ts
@@ -1,3 +1,6 @@
 export const API_BASE = 'http://localhost/api/club';
 
 export const CLUB_ID = '67e54ae51cfd27718dd40be6';
+
+export const CLUB_IVF = '67ee2ca3b35e3c267e3c248d';
+export const CLUB_BOB = '67e54ae51cfd27718dd40bea';
