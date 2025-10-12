45ccbeecb4c949e72eeac3502ee908f326730350
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-10-12 19:24:44 +0900

    feat: useGetCardList가 {clubs, totalCount} 반환하도록 변경

--- frontend/src/hooks/queries/club/useGetCardList.ts
+++ frontend/src/hooks/queries/club/useGetCardList.ts
@@ -3,20 +3,27 @@ import { getClubList } from '@/apis/getClubList';
 import { Club } from '@/types/club';
 import convertToDriveUrl from '@/utils/convertGoogleDriveUrl';
 
+export interface ClubSearchResponse {
+  clubs: Club[];
+  totalCount: number;
+}
+
 export const useGetCardList = (
   keyword: string,
   recruitmentStatus: string,
   category: string,
   division: string,
 ) => {
-  return useQuery<Club[], unknown, Club[]>({
+  return useQuery<ClubSearchResponse, unknown, ClubSearchResponse>({
     queryKey: ['clubs', keyword, recruitmentStatus, division, category],
     queryFn: () => getClubList(keyword, recruitmentStatus, division, category),
     placeholderData: keepPreviousData,
-    select: (data) =>
-      data.map((club) => ({
+    select: (data) => ({
+      totalCount: data.totalCount,
+      clubs: (data.clubs.map((club) => ({
         ...club,
         logo: convertToDriveUrl(club.logo),
-      })),
+      }))),
+    }),
   });
 };
