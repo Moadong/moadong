5cd1cccd4cb1de79ae5b86f89298a9541d9177cc
lepitaaar <leekus1005@gmail.com>
2025-11-09 02:33:14 +0900

    feat: rabbitmq config 파일 추가

---
+++ backend/src/main/java/moadong/global/config/RabbitMQConfig.java
@@ -0,0 +1,42 @@
+package moadong.global.config;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}") private String host;
+    @Value("${spring.rabbitmq.port}") private int port;
+    @Value("${spring.rabbitmq.username}") private String username;
+    @Value("${spring.rabbitmq.password}") private String password;
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(Jackson2JsonMessageConverter());
+
+        return template;
+    }
+
+    @Bean
+    public MessageConverter Jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cf = new CachingConnectionFactory(host, port);
+        cf.setUsername(username);
+        cf.setPassword(password);
+        return cf;
+    }
+}
