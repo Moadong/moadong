e051becdfba7420c687b49a7819adc703336a9c1
lepitaaar <leekus1005@gmail.com>
2025-11-09 02:37:10 +0900

    feat: rabbitmq consumer & publisher 추가

---
+++ backend/src/main/java/moadong/club/summary/ApplicantIdMessagePublisher.java
@@ -0,0 +1,25 @@
+package moadong.club.summary;
+
+import lombok.RequiredArgsConstructor;
+import moadong.club.payload.dto.ApplicationSummaryMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicantIdMessagePublisher {
+    @Value("${rabbitmq.summary.routingKey}")
+    private String routingKey;
+
+    @Value("${rabbitmq.summary.exchange}")
+    private String exchange;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void addApplicantIdToQueue(String applicationFormId, String applicantId) {
+        ApplicationSummaryMessage message = new ApplicationSummaryMessage(applicationFormId, applicantId);
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
