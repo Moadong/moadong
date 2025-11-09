8ae47c044f41a113f16a8255d3dca56a1248aca8
lepitaaar <leekus1005@gmail.com>
2025-11-09 21:48:00 +0900

    feat: add transaction

--- backend/src/main/java/moadong/club/service/ClubApplyPublicService.java
+++ backend/src/main/java/moadong/club/service/ClubApplyPublicService.java
@@ -21,6 +21,7 @@ import moadong.global.payload.Response;
 import moadong.global.util.AESCipher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.function.Function;
@@ -59,6 +60,7 @@ public class ClubApplyPublicService {
         return Response.ok(clubApplicationFormResponse);
     }
 
+    @Transactional
     public void applyToClub(String clubId, String applicationFormId, ClubApplyRequest request) {
         ClubApplicationForm clubApplicationForm = clubApplicationFormsRepository.findByClubIdAndId(clubId, applicationFormId).orElseThrow(() -> new RestApiException(ErrorCode.APPLICATION_NOT_FOUND));
 
