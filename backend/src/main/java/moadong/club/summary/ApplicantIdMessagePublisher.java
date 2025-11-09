d3c9891978326fb6784468aec32205cb3d9ecb78
lepitaaar <leekus1005@gmail.com>
2025-11-09 20:18:24 +0900

    refactor: rabbitmq config 설정에 queue, binding, exchange 셋업.

--- backend/src/main/java/moadong/club/summary/ApplicantIdMessagePublisher.java
+++ backend/src/main/java/moadong/club/summary/ApplicantIdMessagePublisher.java
@@ -3,23 +3,17 @@ package moadong.club.summary;
 import lombok.RequiredArgsConstructor;
 import moadong.club.payload.dto.ApplicantSummaryMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ApplicantIdMessagePublisher {
-    @Value("${rabbitmq.summary.routingKey}")
-    private String routingKey;
 
-    @Value("${rabbitmq.summary.exchange}")
-    private String exchange;
-
-    private final RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate applicantIdTemplate;
 
     public void addApplicantIdToQueue(String applicationFormId, String applicantId) {
         ApplicantSummaryMessage message = new ApplicantSummaryMessage(applicationFormId, applicantId);
 
-        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+        applicantIdTemplate.convertAndSend(message);
     }
 }
--- backend/src/main/java/moadong/global/config/RabbitMQConfig.java
+++ backend/src/main/java/moadong/global/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package moadong.global.config;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,6 +11,8 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 @EnableRabbit
@@ -19,8 +23,64 @@ public class RabbitMQConfig {
     @Value("${spring.rabbitmq.username}") private String username;
     @Value("${spring.rabbitmq.password}") private String password;
 
+    @Value("${rabbitmq.summary.queue}")
+    private String APPLICANT_ID_QUEUE_NAME;
+
+    @Value("${rabbitmq.summary.exchange}")
+    private String APPLICANT_ID_EXCHANGE_NAME;
+
+    @Value("${rabbitmq.summary.routingKey}")
+    private String APPLICANT_ID_ROUTING_KEY;
+
+    private static final String DEAD_LETTER_EXCHANGE_NAME = "dead.letter.exchange";
+    private static final String DEAD_LETTER_QUEUE_NAME = "dead.letter.queue";
+    private static final String DEAD_LETTER_ROUTING_KEY = "dead.letter.routing.key";
+
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    public Queue applicantIdQueue() {
+        return new Queue(APPLICANT_ID_QUEUE_NAME, true, false, false,
+            Map.of(
+                "x-dead-letter-exchange", DEAD_LETTER_EXCHANGE_NAME,
+                "x-dead-letter-routing-key", DEAD_LETTER_ROUTING_KEY
+            )
+        );
+    }
+
+    @Bean
+    public DirectExchange applicantIdExchange() {
+        return new DirectExchange(APPLICANT_ID_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding applicantIdBinding(Queue applicantIdQueue, DirectExchange applicantIdExchange) {
+        return BindingBuilder.bind(applicantIdQueue).to(applicantIdExchange).with(APPLICANT_ID_ROUTING_KEY);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return new Queue(DEAD_LETTER_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(DEAD_LETTER_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding deadLetterBinding(Queue deadLetterQueue, DirectExchange deadLetterExchange) {
+        return BindingBuilder.bind(deadLetterQueue).to(deadLetterExchange).with(DEAD_LETTER_ROUTING_KEY);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate applicantIdTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(Jackson2JsonMessageConverter());
 
