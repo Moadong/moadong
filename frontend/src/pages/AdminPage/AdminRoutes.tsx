0d0b30dc7662e5775d8b89b3c38946bbf5474b8c
lepitaaar <leekus1005@gmail.com>
2025-11-15 16:37:18 +0900

    chord: 번들링 code splitting

---
+++ frontend/src/pages/AdminPage/AdminRoutes.tsx
@@ -0,0 +1,32 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import AdminPage from '@/pages/AdminPage/AdminPage';
+import ClubInfoEditTab from '@/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab';
+import RecruitEditTab from '@/pages/AdminPage/tabs/RecruitEditTab/RecruitEditTab';
+import AccountEditTab from '@/pages/AdminPage/tabs/AccountEditTab/AccountEditTab';
+import ApplicationEditTab from '@/pages/AdminPage/tabs/ApplicationEditTab/ApplicationEditTab';
+import ApplicationListTab from '@/pages/AdminPage/tabs/ApplicationListTab/ApplicationListTab';
+import PhotoEditTab from '@/pages/AdminPage/tabs/PhotoEditTab/PhotoEditTab';
+import ApplicantsTab from '@/pages/AdminPage/tabs/ApplicantsTab/ApplicantsTab';
+import ApplicantDetailPage from '@/pages/AdminPage/tabs/ApplicantsTab/ApplicantDetailPage/ApplicantDetailPage';
+
+export default function AdminRoutes() {
+  return (
+    <Routes>
+      <Route path='' element={<AdminPage />}>
+        <Route index element={<Navigate to='club-info' replace />} />
+        <Route path='club-info' element={<ClubInfoEditTab />} />
+        <Route path='recruit-edit' element={<RecruitEditTab />} />
+        <Route path='photo-edit' element={<PhotoEditTab />} />
+        <Route path='account-edit' element={<AccountEditTab />} />
+        <Route path='application-list' element={<ApplicationListTab />} />
+        <Route path='application-list/edit' element={<ApplicationEditTab />} />
+        <Route path='applicants' element={<ApplicantsTab />} />
+        <Route
+          path='applicants/:questionId'
+          element={<ApplicantDetailPage />}
+        />
+      </Route>
+    </Routes>
+  );
+}
