0e06a52ca96667b5e6a1b59b07728204f5d05202
lepitaaar <leekus1005@gmail.com>
2025-11-28 15:10:06 +0900

    feat: ApplicationFormId 관리를 위한 zustand store 추가

---
+++ frontend/src/store/useApplicationFormStore.ts
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+import {
+  createJSONStorage,
+  persist,
+  subscribeWithSelector,
+} from 'zustand/middleware';
+
+interface ApplicationFormStore {
+  applicationFormId: string;
+  setApplicationFormId: (id: string) => void;
+}
+
+export const useApplicationFormStore = create<ApplicationFormStore>()(
+  subscribeWithSelector(
+    persist(
+      (set) => ({
+        applicationFormId: '',
+        setApplicationFormId: (id: string) => set({ applicationFormId: id }),
+      }),
+      {
+        name: 'application-form-storage',
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+  ),
+);
+
+export const useApplicationFormId = () => {
+  const applicationFormId = useApplicationFormStore(
+    (state) => state.applicationFormId,
+  );
+  const setApplicationFormId = useApplicationFormStore(
+    (state) => state.setApplicationFormId,
+  );
+  return { applicationFormId, setApplicationFormId };
+};
