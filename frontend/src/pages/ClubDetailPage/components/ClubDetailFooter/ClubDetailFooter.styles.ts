f239e68b159b583311cb5a432c895befbeb81f11
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-30 18:41:49 +0900

    refactor: 모달 가림 현상 임시 조치

--- frontend/src/pages/ClubDetailPage/components/ClubDetailFooter/ClubDetailFooter.styles.ts
+++ frontend/src/pages/ClubDetailPage/components/ClubDetailFooter/ClubDetailFooter.styles.ts
@@ -1,11 +1,10 @@
-import { Z_INDEX } from '@/styles/zIndex';
 import styled from 'styled-components';
 
 export const ClubDetailFooterContainer = styled.div`
   position: sticky;
   bottom: 0;
   width: 100%;
-  z-index: ${Z_INDEX.sticky};
+  z-index: 1100; // TODO: Portal로 모달 분리 후 header보다 낮게 재조정
   padding: 10px 40px;
 
   display: flex;
