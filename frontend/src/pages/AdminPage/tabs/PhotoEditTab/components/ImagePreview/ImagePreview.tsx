6393e620b0d4a793137f38a2639d9c85bce653d7
Junseo Kim <semiwest2@gmail.com>
2025-11-30 18:27:25 +0900

    refactor: 피드 이미지 업로드를 새로운 mutation hook으로 전환
    1. 기존의 useUpdateFeedImages, useCreateFeedImage 대신
    useUploadFeed, useUpdateFeed 훅을 사용하도록 변경
    2. 더 이상 사용하지 않는 ImageUpload 컴포넌트를 제거

--- frontend/src/pages/AdminPage/tabs/PhotoEditTab/components/ImagePreview/ImagePreview.tsx
+++ frontend/src/pages/AdminPage/tabs/PhotoEditTab/components/ImagePreview/ImagePreview.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
 import * as Styled from './ImagePreview.styles';
 import delete_button_icon from '@/assets/images/icons/delete_button_icon.svg';
+
 interface ImagePreviewProps {
   image: string;
   onDelete: () => void;
+  disabled?: boolean;
 }
 
-export const ImagePreview = ({ image, onDelete }: ImagePreviewProps) => {
+export const ImagePreview = ({
+  image,
+  onDelete,
+  disabled = false,
+}: ImagePreviewProps) => {
   return (
     <Styled.ImagePreviewContainer>
       <img src={image} alt='preview' />
-      <Styled.DeleteButton onClick={onDelete}>
+      <Styled.DeleteButton
+        onClick={disabled ? undefined : onDelete}
+        disabled={disabled}
+        style={{
+          opacity: disabled ? 0.5 : 1,
+          cursor: disabled ? 'not-allowed' : 'pointer',
+        }}
+      >
         <img src={delete_button_icon} alt='삭제' />
       </Styled.DeleteButton>
     </Styled.ImagePreviewContainer>
