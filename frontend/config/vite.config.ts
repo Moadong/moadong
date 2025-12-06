fe7e5685b758b21a8c1ace4d0e95b116d58068c4
seongwon seo <seongwon0903@gmail.com>
2025-12-06 23:57:05 +0900

    chore: chore: React 핵심 라이브러리를 react-vendor 청크로 분리  - React, ReactDOM, React Router DOM을 'react-vendor' 청크로 별도 분리 - 핵심 라이브러리의 캐시 효율성을 높이고 리소스 병렬 다운로드를 통해 초기 로딩 성능 개선

--- frontend/config/vite.config.ts
+++ frontend/config/vite.config.ts
@@ -9,4 +9,25 @@ export default defineConfig({
   server: {
     port: DEFAULT_PORT,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (
+              id.includes('react') ||
+              id.includes('react-dom') ||
+              id.includes('react-router-dom')
+            ) {
+              return 'react-vendor';
+            }
+            // if (id.includes('@tanstack') || id.includes('framer-motion')) {
+            //   return 'libs-vendor';
+            // }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
