cee2e923cca6c3d34d5fe2539522add818b98a14
seongwon seo <seongwon0903@gmail.com>
2025-12-06 19:43:55 +0900

    feat: 관리자로그인 버튼 클릭 이벤트 추가

--- frontend/src/constants/eventName.ts
+++ frontend/src/constants/eventName.ts
@@ -8,6 +8,7 @@ export const USER_EVENT = {
   MOBILE_HOME_BUTTON_CLICKED: 'Mobile Home Button Clicked',
   MOBILE_MENU_BUTTON_CLICKED: 'Mobile Menu Button Clicked',
   MOBILE_MENU_DELETE_BUTTON_CLICKED: 'Mobile Menubar delete Button Clicked',
+  ADMIN_BUTTON_CLICKED: 'Admin Button Clicked',
 
   // 탭 & 섹션
   TAB_CLICKED: 'Tab Clicked',
