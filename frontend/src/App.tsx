f8c59158b6221630e580495397cdf768f66874ca
Junseo Kim <semiwest2@gmail.com>
2025-12-23 22:09:52 +0900

    feat: 임시 클럽 상세 페이지2 라우트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { AdminClubProvider } from '@/context/AdminClubContext';
 import LoginTab from '@/pages/AdminPage/auth/LoginTab/LoginTab';
 import PrivateRoute from '@/pages/AdminPage/auth/PrivateRoute/PrivateRoute';
 import ClubDetailPage from '@/pages/ClubDetailPage/ClubDetailPage';
+import ClubDetailPage2 from '@/pages/clubDetailPage2/ClubDetailPage2';
 import MainPage from '@/pages/MainPage/MainPage';
 import GlobalStyles from '@/styles/Global.styles';
 import { theme } from '@/styles/theme';
@@ -40,6 +41,14 @@ const App = () => {
               element={
                 <Suspense fallback={null}>
                   <ClubDetailPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path='/club2/:clubId'
+              element={
+                <Suspense fallback={null}>
+                  <ClubDetailPage2 />
                 </Suspense>
               }
             />
