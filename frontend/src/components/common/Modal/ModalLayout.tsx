29287f20d506f210fc05c7494a3b88d8ee55342d
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-01-05 01:22:25 +0900

    refactor: 기존 Modal에서 레이아웃 전용 컴포넌트로 분리

---
+++ frontend/src/components/common/Modal/ModalLayout.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import * as Styled from "./Modal.styles";
+
+interface ModalLayoutProps {
+    onClose?: () => void;
+    title?: string;
+    description?: string;
+    children: ReactNode;
+}
+
+const ModalLayout = ({
+    onClose,
+    title,
+    description,
+    children,
+}: ModalLayoutProps) => {
+    return (
+    <>
+    {(title || onClose) && (
+        <Styled.Header>
+            {title && <Styled.Title>{title}</Styled.Title>}
+            <Styled.IconButton aria-label="close" type="button" onClick={onClose}>
+                ✕
+            </Styled.IconButton>
+        </Styled.Header>
+      )}
+      {description && <Styled.Description>{description}</Styled.Description>}
+      <Styled.Body>{children}</Styled.Body>
+    </>
+  );
+};
+
+export default ModalLayout;
