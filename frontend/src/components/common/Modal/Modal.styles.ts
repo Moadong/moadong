ec0d3e27afa291f62f6c80867993160f1c4685a9
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-01-10 01:08:32 +0900

    fix: 포탈 모달 분리 후 z-index 제거

--- frontend/src/components/common/Modal/Modal.styles.ts
+++ frontend/src/components/common/Modal/Modal.styles.ts
@@ -14,7 +14,6 @@ export const Overlay = styled.div`
 
 export const ContentWrapper = styled.div`
   position: relative;
-  z-index: ${Z_INDEX.modal};
   outline: none;
   display: flex;
   justify-content: center;
