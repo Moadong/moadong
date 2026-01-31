70dd24d6ec4f0d63a918375b0136ad6ef7d062c1
seongwon seo <seongwon0903@gmail.com>
2026-01-31 17:23:06 +0900

    feat: 카테고리버튼 스토리 추가

---
+++ frontend/src/pages/MainPage/components/CategoryButtonList/CategoryButtonList.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CategoryButtonList from './CategoryButtonList';
+import { useCategoryStore } from '@/store/useCategoryStore';
+import { useSearchStore } from '@/store/useSearchStore';
+
+const resetStores = () => {
+  useCategoryStore.setState({ selectedCategory: 'all' });
+  useSearchStore.setState({ keyword: '', inputValue: '', isSearching: false });
+  sessionStorage.removeItem('category-storage');
+};
+
+const meta = {
+  title: 'Pages/MainPage/Components/CategoryButtonList',
+  component: CategoryButtonList,
+  parameters: {
+    layout: 'fullscreen',
+    docs: {
+      description: {
+        component: '카테고리 필터 버튼 목록입니다.',
+      },
+    },
+  },
+  decorators: [
+    (Story) => {
+      resetStores();
+      return <Story />;
+    },
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof CategoryButtonList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
