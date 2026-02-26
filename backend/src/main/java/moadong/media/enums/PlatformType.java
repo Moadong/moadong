3a40d852140891d760fb8cf0899e7f2d2796a935
lepitaaar <leekus1005@gmail.com>
2026-02-27 00:55:12 +0900

    refactor: 불필요한 constructor 제거

--- backend/src/main/java/moadong/media/enums/PlatformType.java
+++ backend/src/main/java/moadong/media/enums/PlatformType.java
@@ -4,13 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum PlatformType {
-    WEB("WEB"),
-    APP_HOME("APP_HOME");
-
-    private final String type;
-
-    PlatformType(String type) {
-        this.type = type;
-    }
-
+    WEB,
+    APP_HOME;
 }
