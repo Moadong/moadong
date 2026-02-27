e0ee335cbf81bc75f5e1c51fbd911ca8dc2042ef
lepitaaar <leekus1005@gmail.com>
2026-02-28 00:56:44 +0900

    refactor: 변경된 타입에 맞게 리팩토링

--- backend/src/main/java/moadong/media/dto/BannerImagesRequest.java
+++ backend/src/main/java/moadong/media/dto/BannerImagesRequest.java
@@ -1,6 +1,6 @@
 package moadong.media.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import moadong.media.enums.PlatformType;
 
@@ -8,7 +8,7 @@ import java.util.List;
 
 public record BannerImagesRequest(
     @NotNull(message = "배너 이미지 목록은 null일 수 없습니다.")
-    List<@NotBlank(message = "배너 이미지 URL은 비어 있을 수 없습니다.") String> images,
+    List<@Valid BannerItemRequest> images,
     @NotNull PlatformType type
 ) {
 }
