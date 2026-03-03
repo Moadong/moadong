5a5711b8e267dc2d02ae1e07f2f1be547591edc0
lepitaaar <leekus1005@gmail.com>
2026-03-03 14:49:31 +0900

    chore: 의존성 정리

--- backend/src/main/java/moadong/media/util/S3Config.java
+++ backend/src/main/java/moadong/media/util/S3Config.java
@@ -11,8 +11,6 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 public class S3Config {
 
