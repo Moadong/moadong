996f9acac6b5ec05f55632121b311007a6226459
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-03-05 04:07:18 +0900

    style: Dday Glass 효과 추가

--- frontend/src/pages/PromotionPage/components/PromotionCard/DdayBadge/DdayBadge.styles.ts
+++ frontend/src/pages/PromotionPage/components/PromotionCard/DdayBadge/DdayBadge.styles.ts
@@ -2,14 +2,24 @@ import { colors } from '@/styles/theme/colors';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  border-radius: 50px;
-  background-color: ${colors.base.white};
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
   padding: 4px 10px;
-  // Glass 효과는 이후 적용 예정
+  border-radius: 50px;
+
+  /* Glass 효과 */
+  background: rgba(245, 245, 245, 0.6);
+  backdrop-filter: blur(12px);
+  box-shadow:
+    inset 0 1px 1px rgb(255, 255, 255),
+    0 2px 8px rgba(0, 0, 0, 0.08);
 `;
 
 export const DdayText = styled.h1`
  color: ${colors.gray[800]};
  font-size: 10px;
  font-weight: 600;
+ letter-spacing: -0.02em;
 `;
