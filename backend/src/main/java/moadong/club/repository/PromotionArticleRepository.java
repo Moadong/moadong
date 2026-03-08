3dff955fb3d89eaae2efe5d66e05d13f6cc3d8e8
lepitaaar <leekus1005@gmail.com>
2026-03-08 16:48:43 +0900

    refactor: change to jpa expression

--- backend/src/main/java/moadong/club/repository/PromotionArticleRepository.java
+++ backend/src/main/java/moadong/club/repository/PromotionArticleRepository.java
@@ -1,9 +1,7 @@
 package moadong.club.repository;
 
 import moadong.club.entity.PromotionArticle;
-import moadong.club.payload.dto.PromotionArticleDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +9,7 @@ import java.util.List;
 @Repository
 public interface PromotionArticleRepository extends MongoRepository<PromotionArticle, String> {
 
-    @Query(value = "{}", sort = "{ 'createdAt': -1 }")
-    List<PromotionArticleDto> findAllProjectedBy();
+    List<PromotionArticle> findAllByOrderByCreatedAtDesc();
 
     List<PromotionArticle> findByClubIdOrderByCreatedAtDesc(String clubId);
 }
