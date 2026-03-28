3f0780dfe00501697593677684d016ef9c7deebb
seongwon seo <seongwon0903@gmail.com>
2026-03-28 17:00:38 +0900

    refactor: getVariant의 useMemo제거

--- frontend/src/hooks/Experiment/useExperimentVariant.ts
+++ frontend/src/hooks/Experiment/useExperimentVariant.ts
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { experimentRepository } from '@/experiments/ExperimentRepository';
 import type {
   ExperimentDefinition,
@@ -8,9 +7,5 @@ import type {
 export const useExperimentVariant = <V extends ExperimentVariant>(
   experiment: ExperimentDefinition<V>,
 ): V => {
-  return useMemo(
-    () => experimentRepository.getVariant(experiment),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [experiment.key],
-  );
+  return experimentRepository.getVariant(experiment);
 };
