6b1426b5e1d7171af7c540f7b9c8b761686e71a5
root <root@CT101.hair-avior.ts.net>
2026-04-13 10:33:29 +0000

    fix: unused variables 에러 해결 (기술부채 보완)

--- frontend/src/pages/ClubDetailPage/components/ClubApplyButton/ClubApplyButton.tsx
+++ frontend/src/pages/ClubDetailPage/components/ClubApplyButton/ClubApplyButton.tsx
@@ -90,7 +90,8 @@ const ClubApplyButton = ({
       }
       setApplicationOptions(forms);
       setIsApplicationModalOpen(true);
-    } catch (e) {
+    } catch {
+      // alert('지원서 정보를 불러오는 중 오류가 발생했습니다. 다시 시도해주세요.');
       setApplicationOptions([]);
       setIsApplicationModalOpen(true);
     }
