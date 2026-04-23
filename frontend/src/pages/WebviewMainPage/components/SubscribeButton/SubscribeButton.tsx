def2df333b26e0fa0d4d0e2368f017d706027d27
Junseo Kim <semiwest2@gmail.com>
2026-04-23 15:02:50 +0900

    refactor: SubscribeButton 인라인 SVG를 SVGR 임포트로 교체
    구독/비구독 상태별로 별도 SVG 파일을 사용하던 방식에서,
    currentColor를 활용한 단일 SVG 파일(subscribe_button_icon.svg)로 통합.
    색상 전환은 styled-component의 color 속성으로 처리.

--- frontend/src/pages/WebviewMainPage/components/SubscribeButton/SubscribeButton.tsx
+++ frontend/src/pages/WebviewMainPage/components/SubscribeButton/SubscribeButton.tsx
@@ -1,3 +1,4 @@
+import SubscribeIcon from '@/assets/images/icons/subscribe_button_icon.svg?react';
 import * as Styled from '../../WebviewMainPage.styles';
 
 interface SubscribeButtonProps {
@@ -17,21 +18,7 @@ const SubscribeButton = ({ subscribed, onToggle }: SubscribeButtonProps) => {
       onClick={handleClick}
       aria-label={subscribed ? '구독 취소' : '구독'}
     >
-      <svg
-        width='22'
-        height='20'
-        viewBox='0 0 22 20'
-        fill={subscribed ? 'currentColor' : 'none'}
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path
-          d='M11 18.5C11 18.5 1.5 13 1.5 6.5C1.5 4.01472 3.51472 2 6 2C7.74261 2 9.26309 2.97679 10.0919 4.41439C10.3528 4.87204 10.9972 4.87204 11.2581 4.41439C12.0869 2.97679 13.6074 2 15.35 2C17.8353 2 19.85 4.01472 19.85 6.5C19.85 13 10.35 18.5 10.35 18.5'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-      </svg>
+      <SubscribeIcon width={18} height={18} />
     </Styled.SubscribeButton>
   );
 };
