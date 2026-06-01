a4f48e56fc7e3d60b6b689c84f1f7f28ef287bc1
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-06-01 21:29:48 +0900

    fix(map): MapCard를 button 요소로 변경하여 키보드 접근성 개선

--- frontend/src/pages/ClubDetailPage/ClubDetailPage.styles.ts
+++ frontend/src/pages/ClubDetailPage/ClubDetailPage.styles.ts
@@ -56,7 +56,9 @@ export const MapInfo = styled.div`
   }
 `;
 
-export const MapCard = styled.div`
+export const MapCard = styled.button`
+  padding: 0;
+  font: inherit;
   width: 100%;
   height: 189px;
 
--- frontend/src/pages/PromotionPage/components/detail/PromotionMapSection/PromotionMapSection.styles.ts
+++ frontend/src/pages/PromotionPage/components/detail/PromotionMapSection/PromotionMapSection.styles.ts
@@ -10,7 +10,9 @@ export const Container = styled.section`
   }
 `;
 
-export const MapCard = styled.div`
+export const MapCard = styled.button`
+  padding: 0;
+  font: inherit;
   width: 100%;
   height: 189px;
   border-radius: 20px;
