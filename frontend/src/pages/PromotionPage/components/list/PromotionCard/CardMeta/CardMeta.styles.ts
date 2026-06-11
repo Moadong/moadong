70df34d259f92a58a318658c7c132c117af904fc
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-06-11 20:21:28 +0900

    fix(promotion): 홍보 카드 제목 최대 2줄 표시로 변경

--- frontend/src/pages/PromotionPage/components/list/PromotionCard/CardMeta/CardMeta.styles.ts
+++ frontend/src/pages/PromotionPage/components/list/PromotionCard/CardMeta/CardMeta.styles.ts
@@ -13,7 +13,9 @@ export const Title = styled.h3`
   min-width: 0;
   overflow: hidden;
   text-overflow: ellipsis;
-  white-space: nowrap;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
   margin-bottom: 4px;
 
   ${media.mini_mobile} {
