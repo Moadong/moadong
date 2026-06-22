3e03235f3c4eb556a2f5e61d148fa7382d4591d1
seongwon seo <seongwon0903@gmail.com>
2026-06-22 21:04:42 +0900

    style: prettier 포맷팅 적용

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = import.meta.env.DEV
-  ? (typeof window !== 'undefined'
-      ? window.location.origin
-      : import.meta.env.VITE_API_BASE_URL)
+  ? typeof window !== 'undefined'
+    ? window.location.origin
+    : import.meta.env.VITE_API_BASE_URL
   : import.meta.env.VITE_API_BASE_URL;
 export default API_BASE_URL;
