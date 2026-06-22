58ff9c8e47c1cc655d9f2fa844184aace765b0aa
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-06-23 04:28:09 +0900

    feat(components): WebviewTopBar onBack prop 추가

--- frontend/src/components/common/WebviewTopBar/WebviewTopBar.tsx
+++ frontend/src/components/common/WebviewTopBar/WebviewTopBar.tsx
@@ -7,14 +7,19 @@ import * as Styled from './WebviewTopBar.styles';
 
 interface Props {
   title: string;
+  onBack?: () => void;
 }
 
-const WebviewTopBar = ({ title }: Props) => {
+const WebviewTopBar = ({ title, onBack }: Props) => {
   const navigate = useNavigate();
   const trackEvent = useMixpanelTrack();
 
   const handleBackClick = () => {
     trackEvent(USER_EVENT.BACK_BUTTON_CLICKED);
+    if (onBack) {
+      onBack();
+      return;
+    }
     const handled = requestNavigateBack();
     if (!handled) {
       if (window.history.state && window.history.state.idx > 0) {
