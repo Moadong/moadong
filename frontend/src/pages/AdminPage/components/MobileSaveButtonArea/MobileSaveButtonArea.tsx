109523ec9fcb1eea0254b482942867991a038119
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-06-23 04:28:33 +0900

    feat(admin): MobileSaveButtonArea 공통 컴포넌트 추출

---
+++ frontend/src/pages/AdminPage/components/MobileSaveButtonArea/MobileSaveButtonArea.tsx
@@ -0,0 +1,17 @@
+import Button from '@/components/common/Button/Button';
+import * as Styled from './MobileSaveButtonArea.styles';
+
+interface MobileSaveButtonAreaProps {
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const MobileSaveButtonArea = ({ onClick, disabled }: MobileSaveButtonAreaProps) => (
+  <Styled.SaveButtonArea>
+    <Button onClick={onClick} disabled={disabled}>
+      저장하기
+    </Button>
+  </Styled.SaveButtonArea>
+);
+
+export default MobileSaveButtonArea;
