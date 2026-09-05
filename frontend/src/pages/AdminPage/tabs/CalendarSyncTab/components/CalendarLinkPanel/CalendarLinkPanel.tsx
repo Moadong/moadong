d9393ad2f5954f7bd2f39687ba31eb244e50f49d
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2026-09-06 02:36:42 +0900

    refactor(modal): FeedbackConfirmModal, DisconnectConfirmModal을 공통 ConfirmModal로 교체

--- frontend/src/pages/AdminPage/tabs/CalendarSyncTab/components/CalendarLinkPanel/CalendarLinkPanel.tsx
+++ frontend/src/pages/AdminPage/tabs/CalendarSyncTab/components/CalendarLinkPanel/CalendarLinkPanel.tsx
@@ -14,7 +14,7 @@ import CalendarLinkCard, {
   type CalendarLinkEvent,
 } from '../CalendarLinkCard/CalendarLinkCard';
 import CalendarLinkSection from '../CalendarLinkSection/CalendarLinkSection';
-import DisconnectConfirmModal from '../DisconnectConfirmModal/DisconnectConfirmModal';
+import ConfirmModal from '@/components/common/ConfirmModal/ConfirmModal';
 import * as Styled from './CalendarLinkPanel.styles';
 
 /** 목록에 표시할 `YYYY. MM. DD` 텍스트로 변환한다 */
@@ -180,8 +180,11 @@ const CalendarLinkPanel = () => {
         isToggleDisabled={isVisibilityUnknown}
       />
 
-      <DisconnectConfirmModal
+      <ConfirmModal
         isOpen={disconnectTarget !== null}
+        title='연동을 해제할까요?'
+        description='가져온 일정은 캘린더에서 사라집니다.'
+        confirmLabel='확인'
         onClose={() => {
           trackEvent(ADMIN_EVENT.CALENDAR_UNLINK_CANCELED, {
             provider: disconnectTarget,
