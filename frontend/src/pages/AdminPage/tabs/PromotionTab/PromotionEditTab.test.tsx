1bb7855e7b7f40736891486f3444194105b41034
seongwon seo <seongwon0903@gmail.com>
2026-09-17 21:26:15 +0900

    style: prettier 포맷을 적용한다

--- frontend/src/pages/AdminPage/tabs/PromotionTab/PromotionEditTab.test.tsx
+++ frontend/src/pages/AdminPage/tabs/PromotionTab/PromotionEditTab.test.tsx
@@ -27,10 +27,9 @@ const mockDevice = {
   isDesktop: true,
 };
 jest.mock('@/hooks/useDevice', () => () => mockDevice);
-jest.mock(
-  '@/components/map/MapLocationPicker/MapLocationPicker',
-  () => () => <div />,
-);
+jest.mock('@/components/map/MapLocationPicker/MapLocationPicker', () => () => (
+  <div />
+));
 // react-datepicker의 css import를 jest가 파싱하지 못해 통째로 대체한다
 jest.mock(
   '@/pages/AdminPage/tabs/RecruitEditTab/components/DateTimeRangePicker/DateTimeRangePicker',
